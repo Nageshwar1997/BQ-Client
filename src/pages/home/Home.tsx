ca5235f8f83ea7415128ad396eae4c094ce99b56
nageshwarp1997 <nageshwar@ctruh.com>
2025-03-29 01:04:57 +0530

    Removed Cloudinary

--- src/pages/home/Home.tsx
+++ src/pages/home/Home.tsx
@@ -1,12 +1,9 @@
-import CloudinaryVideoCarousel from "./components/CloudinaryVideoCarousel";
-// import VideoCarousel from "./components/VideoCarousel";
+import VideoCarousel from "./components/VideoCarousel";
 
 const Home = () => {
   return (
     <div className="w-full h-full lg:-mt-16">
-      <div className="w-full h-[500px]">
-        <CloudinaryVideoCarousel />
-      </div>
+      <VideoCarousel />
     </div>
   );
 };
