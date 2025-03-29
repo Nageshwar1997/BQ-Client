bf850761ff4050289ddb449eb9cd3c54d59115de
nageshwarp1997 <nageshwar@ctruh.com>
2025-03-30 02:02:08 +0530

    `Added HomeHero component to Home page`

--- src/pages/home/Home.tsx
+++ src/pages/home/Home.tsx
@@ -1,9 +1,11 @@
+import HomeHero from "./components/HomeHero";
 import VideoCarousel from "./components/VideoCarousel";
 
 const Home = () => {
   return (
     <div className="w-full h-full lg:-mt-16">
       <VideoCarousel />
+      <HomeHero />
     </div>
   );
 };
