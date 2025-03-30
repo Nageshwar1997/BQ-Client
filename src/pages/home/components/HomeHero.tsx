ca654e0dd953c7fcfe5779a033df971dd875dadb
nageshwarp1997 <nageshwar@ctruh.com>
2025-03-30 19:04:11 +0530

    `Refactored HomeHero component and removed BgGradient component`

--- src/pages/home/components/HomeHero.tsx
+++ src/pages/home/components/HomeHero.tsx
@@ -1,72 +1,39 @@
-import useThemeStore from "../../../store/theme.store";
 import { Link } from "react-router-dom";
 import Button from "../../../components/button/Button";
 import { RightArrowIcon } from "../../../icons";
-import BgGradient from "./BgGradient";
 import LineGrid from "../../../components/ui/LineGrid";
 
 const HomeHero = () => {
-  const { theme } = useThemeStore();
-
   return (
-    <div className="relative flex flex-col justify-between overflow-hidden lg:h-[70dvh] border z-0 ">
-      {/* Pink gradient */}
-      <BgGradient className="absolute bottom-0 w-screen object-cover -z-10" />
-      <div className="flex justify-center items-center flex-col lg:flex-row gap-[10%] lg:px-28 min-[1280px]:px-12 lg:pt-[10%] max-[360px]:px-3 md:pt-[18%] pt-[25%] max-[375px]:h-auto h-screen lg:h-auto ">
-        <div className="flex flex-col items-start md:items-center lg:items-start">
-          {/* Award */}
-          <div className="mb-6 rounded-full py-2 pl-3 pr-4 border border-light home-announcement-chip flex items-center gap-2.5">
-            <img
-              alt=""
-              src={
-                theme === "dark"
-                  ? "/images/home/announcement.png"
-                  : "/images/home/announcement-light-mode.png"
-              }
-              width={0}
-              height={0}
-              sizes="100vw"
-              className="md:w-[237px] w-[180px] relative  h-full object-contain border"
+    <div className="relative w-full h-fit overflow-hidden py-5 bg-gradient-to-t from-[#fe026b] to-transparent">
+      <div className="flex justify-center items-center flex-col lg:flex-row gap-5 px-3 lg:px-16 py-5">
+        <div className="flex flex-col items-center lg:items-start text-center lg:text-left w-full">
+          <h1 className="text-3xl md:text-4xl leading-[48px] md:leading-[52px] lg:text-left font-semibold">
+            <span className="bg-silver-duo bg-clip-text text-transparent">
+              India's First Beauty Brand that Delivers Products Directly to the
+              Customer
+            </span>
+          </h1>
+          <h1 className="bg-silver-duo bg-clip-text text-transparent font-semibold tracking-wide py-5 text-4xl sm:text-5xl md:text-7xl">
+            BEAUTINIQUE
+          </h1>
+          <h1 className="font-medium md:text-4xl text-3xl tracking-[16px] lg:tracking-[20px] opacity-80">
+            ON THE WEB
+          </h1>
+          <Link to="/register" className="mt-8">
+            <Button
+              pattern="primary"
+              content={"Register Now"}
+              rightIcon={<RightArrowIcon className="w-5 h-5 fill-white" />}
+              className="py-4 !px-6 !gap-3"
             />
-            {/* <CtruhLogoIconGradient className="w-[55px]" /> */}
-          </div>
-          <div className="flex flex-col items-center lg:items-start text-center lg:text-left w-full">
-            <h1 className="font-degular md:text-[40px]  leading-[52px] text-4xl lg:text-left font-semibold">
-              <span className="bg-silver-duo bg-clip-text text-transparent lg:mr-2">{`World’s First`}</span>
-              <br className="lg:hidden" />
-              <span className="bg-silver-duo bg-clip-text text-transparent">
-                No-Code/Low-Code
-              </span>
-            </h1>
-            <h1 className="bg-silver-duo bg-clip-text text-transparent  max-[360px]:text-[38px] md:text-[64px] text-[40px] font-semibold py-3 md:leading-[77px] leading-[48px]">
-              3D ENGINE
-            </h1>
-            <h1 className="font-medium md:text-4xl text-3xl max-[360px]:text-[24px] tracking-[18px] lg:tracking-[22px] opacity-80">
-              ON THE WEB
-            </h1>
-            <Link to="/register" className="mt-8">
-              <Button
-                pattern="primary"
-                content={"Get Started"}
-                rightIcon={
-                  <RightArrowIcon
-                    className="w-6 h-6"
-                    // fill="var(--text-default-invert)"
-                  />
-                }
-                // wrapperClassName="w-fit"
-                className="!py-5 !px-6 !gap-[10px]"
-              />
-            </Link>
-          </div>
+          </Link>
         </div>
-
-        <div className="relative flex items-center justify-center w-[518px] h-[351px] lg:mb-0 pointer-events-none lg:pointer-events-auto">
+        <div className="relative flex items-center justify-center w-[518px] h-[351px]">
           <LineGrid className="absolute -top-10 -right-10 [&_line]:stroke-primary-50 [&_line]:opacity-50" />
-          <div className='absolute md:h-[351px] md:w-[376px] w-[280px] h-[280px] aspect-square cursor-pointer  bg-[url("/images/logo/BQ.webp")] bg-cover bg-center bg-no-repeat bg-opacity-50 border'></div>
+          <div className='max-w-[300px] max-h-[280px] md:max-w-[370px] md:max-h-[340px] w-full h-full bg-[url("/images/logo/BQ.webp")] bg-cover bg-center bg-no-repeat bg-opacity-50 border' />
         </div>
       </div>
-      {/* <BrandIcons isFade={true} /> */}
     </div>
   );
 };
