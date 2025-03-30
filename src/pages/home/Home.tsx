8991f8efd5478cf20475a5715646eb71bc3bc589
nageshwarp1997 <nageshwar@ctruh.com>
2025-03-30 21:27:48 +0530

    Heading With Description Component created

--- src/pages/home/Home.tsx
+++ src/pages/home/Home.tsx
@@ -1,11 +1,13 @@
+import CategoriesGrid from "./components/CategoriesGrid";
 import HomeHero from "./components/HomeHero";
 import VideoCarousel from "./components/VideoCarousel";
 
 const Home = () => {
   return (
-    <div className="w-full h-full lg:-mt-16">
+    <div className="w-full h-full space-y-10 lg:-mt-16">
       <VideoCarousel />
       <HomeHero />
+      <CategoriesGrid />
     </div>
   );
 };
