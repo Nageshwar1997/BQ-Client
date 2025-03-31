8fd5cff784d79c0943cc4d64020d4d03b2149f74
nageshwarp1997 <nageshwar@ctruh.com>
2025-03-31 23:27:40 +0530

    `Update CategoryCard component: remove shadow-light-dark-soft class from div and adjust opacity transition duration and value on hover`

--- src/pages/home/components/categoriesGrid/childrens/CategoryCard.tsx
+++ src/pages/home/components/categoriesGrid/childrens/CategoryCard.tsx
@@ -42,11 +42,11 @@ const CategoryCard = memo(
 
     return (
       <div
-        className={`relative border-rounded-corners-gradient shadow-light-dark-soft bg-black cursor-pointer rounded-3xl overflow-hidden group hover:shadow-xl ${className}`}
+        className={`relative border-rounded-corners-gradient shadow-light-dark-soft cursor-pointer rounded-3xl overflow-hidden group hover:shadow-xl ${className}`}
         onMouseEnter={stopImageRotation}
         onMouseLeave={startImageRotation}
       >
-        <div className="absolute bottom-0 left-0 w-full bg-gradient-to-t from-primary-inverted to-transparent px-6 pt-8 pb-6 flex flex-col items-center justify-center z-[1] group-hover:opacity-0 transition-opacity duration-500">
+        <div className="absolute bottom-0 left-0 w-full bg-gradient-to-t from-primary-inverted to-transparent px-6 pt-8 pb-6 flex flex-col items-center justify-center z-[1] group-hover:opacity-70 transition-opacity duration-500">
           <h3 className="text-xl font-medium leading-6 mb-2 text-secondary">
             {title}
           </h3>
