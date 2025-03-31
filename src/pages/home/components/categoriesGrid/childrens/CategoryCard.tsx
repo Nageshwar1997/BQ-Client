11d9411a4e1bfc43f377927c04d7bef40a29274d
nageshwarp1997 <nageshwar@ctruh.com>
2025-03-31 23:25:29 +0530

    `Refactor CategoryCard component to use useRef and clearInterval instead of useState and useEffect`

--- src/pages/home/components/categoriesGrid/childrens/CategoryCard.tsx
+++ src/pages/home/components/categoriesGrid/childrens/CategoryCard.tsx
@@ -1,4 +1,4 @@
-import { memo, useEffect, useState } from "react";
+import { memo, useEffect, useRef } from "react";
 
 const CategoryCard = memo(
   ({
@@ -12,26 +12,39 @@ const CategoryCard = memo(
     className?: string;
     images: { img: string }[];
   }) => {
-    const [imageUrl, setImageUrl] = useState(images[0].img);
-    const [isHovered, setIsHovered] = useState(false);
+    const imageRef = useRef<HTMLImageElement>(null);
+    const intervalRef = useRef<number | null>(null);
 
-    useEffect(() => {
-      let intervalId: number;
-
-      if (!isHovered) {
-        intervalId = setInterval(() => {
-          setImageUrl(images[Math.floor(Math.random() * images.length)].img);
+    const startImageRotation = () => {
+      if (!intervalRef.current) {
+        intervalRef.current = setInterval(() => {
+          if (imageRef.current) {
+            const randomImage =
+              images[Math.floor(Math.random() * images.length)].img;
+            imageRef.current.src = randomImage;
+          }
         }, 2000);
       }
+    };
 
-      return () => clearInterval(intervalId);
-    }, [images, isHovered]);
+    const stopImageRotation = () => {
+      if (intervalRef.current) {
+        clearInterval(intervalRef.current);
+        intervalRef.current = null;
+      }
+    };
+
+    useEffect(() => {
+      startImageRotation();
+      return () => stopImageRotation();
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+    }, []);
 
     return (
       <div
         className={`relative border-rounded-corners-gradient shadow-light-dark-soft bg-black cursor-pointer rounded-3xl overflow-hidden group hover:shadow-xl ${className}`}
-        onMouseEnter={() => setIsHovered(true)}
-        onMouseLeave={() => setIsHovered(false)}
+        onMouseEnter={stopImageRotation}
+        onMouseLeave={startImageRotation}
       >
         <div className="absolute bottom-0 left-0 w-full bg-gradient-to-t from-primary-inverted to-transparent px-6 pt-8 pb-6 flex flex-col items-center justify-center z-[1] group-hover:opacity-0 transition-opacity duration-500">
           <h3 className="text-xl font-medium leading-6 mb-2 text-secondary">
@@ -42,7 +55,8 @@ const CategoryCard = memo(
           </p>
         </div>
         <img
-          src={imageUrl}
+          ref={imageRef}
+          src={images[0].img}
           alt={title}
           className="w-full h-72 md:h-[280px] lg:h-[250px] xl:h-72 object-cover transition-transform transform group-hover:scale-105 duration-1000 z-0"
           loading="lazy"
