5b901be3ec7cfe9e377f41e950aa8907c12cef4e
nageshwarp1997 <nageshwar@ctruh.com>
2025-04-01 19:14:44 +0530

    `Update DealForYou component: adjust dots indicator position and size`

--- src/pages/home/components/DealForYou.tsx
+++ src/pages/home/components/DealForYou.tsx
@@ -31,12 +31,12 @@ const DealForYou = () => {
       </div>
 
       {/* Dots Indicator */}
-      <div className="absolute bottom-4 left-1/2 transform -translate-x-1/2 flex space-x-2">
+      <div className="absolute bottom-2 md:bottom-4 right-2 md:right-4 transform flex space-x-2">
         {HOME_DEALS_DATA.map((_, index) => (
           <button
             key={index}
             onClick={() => setCurrentIndex(index)}
-            className={`w-3 h-3 rounded-full transition-all cursor-pointer ${
+            className={`w-2 h-2 md:w-3 md:h-3 rounded-full transition-all cursor-pointer ${
               index === currentIndex ? "bg-primary scale-125" : "bg-tertiary"
             }`}
           />
