32425e540eb7456e41575bbcdcbbe5a82698edb8
nageshwarp1997 <nageshwar@ctruh.com>
2025-04-11 22:43:02 +0530

    Auth Check Issue Fixed

--- src/hooks/useAuthCheck.tsx
+++ src/hooks/useAuthCheck.tsx
@@ -3,21 +3,18 @@ import { useEffect } from "react";
 import { useGetUserDetails } from "../api/user/user.service";
 import { useUserStore } from "../store/user.store";
 import { getUserToken } from "../utils";
-import { useNavigate } from "react-router-dom";
 
 export const useAuthCheck = () => {
-  const { setUser, isAuthenticated, user } = useUserStore();
+  const { setUser, user } = useUserStore();
   const { data, isLoading, isError } = useGetUserDetails();
-  const navigate = useNavigate();
 
   useEffect(() => {
     try {
-      if (getUserToken() && !isAuthenticated && !user && data?.user) {
+      if (getUserToken() && !user && data?.user) {
         setUser(data.user);
       }
-    // eslint-disable-next-line @typescript-eslint/no-unused-vars
     } catch (error) {
-      navigate("/login");
+      console.log("Error in auth check:", error);
     }
     // eslint-disable-next-line react-hooks/exhaustive-deps
   }, [data?.user]);
