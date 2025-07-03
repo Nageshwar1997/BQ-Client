7c431a0d088263f0e1255c4c7c6fecb8a5325f66
nageshwarp1997 <nageshwar@ctruh.com>
2025-07-03 23:09:40 +0530

    Added api routes

---
+++ src/api/api.routes.ts
@@ -0,0 +1,26 @@
+export const authRoutes = {
+  register: { method: "POST", url: "/auth/register" },
+  login: { method: "POST", url: "/auth/login" },
+};
+
+export const productRoutes = {
+  // uploadProduct: { method: "POST", url: "/products/upload" },
+  // updateProduct: { method: "PATCH", url: "/products/product/update" },
+  getAllProducts: { method: "POST", url: "/products/all" },
+  getProductById: { method: "POST", url: "/products/product" },
+  // deleteProduct: { method: "DELETE", url: "/products/product/delete" },
+};
+
+export const userRoutes = {
+  getUser: { method: "GET", url: "/users/user" },
+};
+
+export const mediaRoutes = {
+  // For Images
+  // uploadSingleImage: { method: "POST", url: "/media/image/upload" },
+  // uploadMultipleImages: { method: "POST", url: "/media/images/upload" },
+  // deleteSingleImage: { method: "DELETE", url: "/media/image/delete" },
+  // deleteMultipleImages: { method: "DELETE", url: "/media/images/delete" },
+  // For Videos
+  getHomeVideos: { method: "GET", url: "/media/videos/home" },
+};
