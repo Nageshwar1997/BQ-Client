2629c870dd743cf902853ca6e397381cb7989118
nageshwarp1997 <nageshwar@ctruh.com>
2025-07-04 01:02:24 +0530

    Removed unused code

--- src/api/product/product.service.ts
+++ src/api/product/product.service.ts
@@ -1,27 +1,7 @@
 import { useMutation } from "@tanstack/react-query";
-import { toastErrorMessage, toastSuccessMessage } from "../../utils/toast.util";
-import {
-  delete_product,
-  get_all_products,
-  get_product_by_id,
-  update_product,
-  upload_product,
-} from "./product.api";
+import { get_all_products, get_product_by_id } from "./product.api";
 import { IProductPossibleBodyFields } from "../../types";
-
-export const useUploadProduct = () => {
-  return useMutation({
-    mutationFn: (bodyData: FormData) => upload_product(bodyData),
-    onSuccess: (data) => {
-      toastSuccessMessage(data?.message || "Product uploaded successfully!");
-    },
-    onError: (error: unknown) => {
-      toastErrorMessage(
-        typeof error === "string" ? error : "Failed to upload product!"
-      );
-    },
-  });
-};
+import { toastErrorMessage, toastSuccessMessage } from "../../utils/toasts";
 
 export const useGetAllProducts = () => {
   return useMutation({
@@ -58,35 +38,6 @@ export const useGetProductById = () => {
     onError: (error: unknown) => {
       toastErrorMessage(
         typeof error === "string" ? error : "Failed to fetch product!"
-      );
-    },
-  });
-};
-
-export const useUpdateProduct = () => {
-  return useMutation({
-    mutationFn: ({ data, productId }: { data: FormData; productId: string }) =>
-      update_product({ data, productId }),
-    onSuccess: (data) => {
-      toastSuccessMessage(data?.message || "Product updated successfully!");
-    },
-    onError: (error: unknown) => {
-      toastErrorMessage(
-        typeof error === "string" ? error : "Failed to update product!"
-      );
-    },
-  });
-};
-
-export const useDeleteProduct = () => {
-  return useMutation({
-    mutationFn: (productId: string) => delete_product(productId),
-    onSuccess: (data) => {
-      toastSuccessMessage(data?.message || "Product deleted successfully!");
-    },
-    onError: (error: unknown) => {
-      toastErrorMessage(
-        typeof error === "string" ? error : "Failed to delete product!"
       );
     },
   });
