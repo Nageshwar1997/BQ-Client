0bba462652aff78869d016bb22056aef243fb2d1
nageshwarp1997 <nageshwar@ctruh.com>
2025-07-05 23:39:17 +0530

    Props Updated

--- src/components/input/Checkbox.tsx
+++ src/components/input/Checkbox.tsx
@@ -1,13 +1,15 @@
 const Checkbox = ({
   register,
   className = "",
+  labelClassName = "",
 }: {
   register?: object;
   className?: string;
+  labelClassName?: string;
 }) => {
   return (
     <label
-      className={`relative inline-flex items-center cursor-pointer ${className}`}
+      className={`relative inline-flex items-center cursor-pointer ${labelClassName}`}
     >
       <input
         name="remember"
@@ -15,7 +17,9 @@ const Checkbox = ({
         className="sr-only peer outline-none"
         {...register}
       />
-      <div className="w-10 md:w-11 h-5 md:h-6 bg-primary-50 rounded-full peer-checked:after:translate-x-5 after:content-[''] after:absolute after:top-1 after:left-1 after:bg-white after:border-gray-300 after:border after:rounded-full after:h-3 after:w-3 after:md:h-4 after:md:w-4 after:transition-all peer-checked:bg-blue-600" />
+      <div
+        className={`w-10 md:w-11 h-5 md:h-6 bg-primary-50 rounded-full peer-checked:after:translate-x-5 after:content-[''] after:absolute after:top-1 after:left-1 after:bg-white after:border-gray-300 after:border after:rounded-full after:h-3 after:w-3 after:md:h-4 after:md:w-4 after:transition-all peer-checked:bg-blue-600 ${className}`}
+      />
     </label>
   );
 };
