db2affc2868a1abc6e78e8ec00031fb05f7c3808
nageshwarp1997 <nageshwar@ctruh.com>
2025-07-05 18:36:17 +0530

    added className props

--- src/components/input/Checkbox.tsx
+++ src/components/input/Checkbox.tsx
@@ -1,13 +1,21 @@
-const Checkbox = ({ register }: { register: object }) => {
+const Checkbox = ({
+  register,
+  className = "",
+}: {
+  register?: object;
+  className?: string;
+}) => {
   return (
-    <label className="relative inline-flex items-center cursor-pointer">
+    <label
+      className={`relative inline-flex items-center cursor-pointer ${className}`}
+    >
       <input
         name="remember"
         type="checkbox"
         className="sr-only peer outline-none"
         {...register}
       />
-      <div className="w-10 md:w-11 h-5 md:h-6 bg-primary-50 rounded-full peer-checked:after:translate-x-5 after:content-[''] after:absolute after:top-1 after:left-1 after:bg-white after:border-gray-300 after:border after:rounded-full after:h-3 after:w-3 after:md:h-4 after:md:w-4 after:transition-all peer-checked:bg-blue-600"></div>
+      <div className="w-10 md:w-11 h-5 md:h-6 bg-primary-50 rounded-full peer-checked:after:translate-x-5 after:content-[''] after:absolute after:top-1 after:left-1 after:bg-white after:border-gray-300 after:border after:rounded-full after:h-3 after:w-3 after:md:h-4 after:md:w-4 after:transition-all peer-checked:bg-blue-600" />
     </label>
   );
 };
