6171e100bcdc2d43e2935c5b9987ce17ad6a8fd0
nageshwarp1997 <nageshwar@ctruh.com>
2025-07-07 00:12:28 +0530

    Icons updated

--- src/icons/index.tsx
+++ src/icons/index.tsx
@@ -674,6 +674,26 @@ export const FilterIcon = ({ className }: IconProps) => (
   </svg>
 );
 
+export const UpDownArrowIcon = ({ className }: IconProps) => (
+  <svg
+    stroke="black"
+    xmlns="http://www.w3.org/2000/svg"
+    width="24"
+    height="24"
+    fill="none"
+    strokeWidth="2"
+    viewBox="0 0 24 24"
+    stroke-linecap="round"
+    stroke-linejoin="round"
+    className={className}
+  >
+    <path d="m3 16 4 4 4-4" />
+    <path d="M7 20V4" />
+    <path d="m21 8-4-4-4 4" />
+    <path d="M17 4v16" />
+  </svg>
+);
+
 export const PercentIcon = ({ className }: IconProps) => (
   <svg
     xmlns="http://www.w3.org/2000/svg"
