a5e4694c2fd735a1cfce3a3722537fcf2623f18c
nageshwarp1997 <nageshwar@ctruh.com>
2025-07-07 00:13:27 +0530

    SortBy Component added

--- src/pages/product/categoryProducts/SortBy.tsx
+++ src/pages/product/categoryProducts/SortBy.tsx
@@ -1,9 +1,38 @@
-const SortBy = ({ className = "" }: { className?: string }) => {
-  return (
-    <div className={`w-[400px] h-[500px] border border-[red] ${className}`}>
-      SortBy
-    </div>
-  );
-};
+import useQueryParams from "../../../hooks/useQueryParams";
+import { SORT_BY_OPTIONS } from "./data";
 
-export default SortBy;
+interface SortByProps {
+  className?: string;
+}
+
+function Filters({ className = "" }: SortByProps) {
+  const { queryParams, setParams, removeParam } = useQueryParams();
+  const current = queryParams.sortBy;
+  return (
+    <section
+      className={`h-full flex gap-6 bg-primary-inverted select-none ${className}`}
+    >
+      <ul className={`w-full flex flex-col gap-1 p-4`}>
+        {SORT_BY_OPTIONS.map((opt) => (
+          <li
+            key={opt.value}
+            onClick={() =>
+              current === opt.value
+                ? removeParam("sortBy")
+                : setParams({ sortBy: opt.value })
+            }
+            className={`text-sm sm:text-base text-primary font-medium cursor-pointer px-3 py-2 transform hover:-translate-x-2 transition-all hover:duration-300 whitespace-nowrap ${
+              current === opt.value
+                ? "-translate-x-2 text-primary"
+                : "text-tertiary"
+            }`}
+          >
+            {opt.label}
+          </li>
+        ))}
+      </ul>
+    </section>
+  );
+}
+
+export default Filters;
--- src/pages/product/categoryProducts/data.ts
+++ src/pages/product/categoryProducts/data.ts
@@ -479,3 +479,14 @@ export const CATEGORY_IMAGE_DATA: CategoryImageDataType = {
   eyes,
   collections,
 };
+
+export const SORT_BY_OPTIONS = [
+  { label: "Featured", value: "featured" },
+  { label: "Best selling", value: "best-selling" },
+  { label: "Alphabetically, A-Z", value: "a-z" },
+  { label: "Alphabetically, Z-A", value: "z-a" },
+  { label: "Price, low to high", value: "price-low-high" },
+  { label: "Price, high to low", value: "price-high-low" },
+  { label: "Date, old to new", value: "date-old-new" },
+  { label: "Date, new to old", value: "date-new-old" },
+];
