5b39abb08dde1869cb7467f6accbe2d29992b576
nageshwarp1997 <nageshwar@ctruh.com>
2025-07-10 10:26:46 +0530

    fix: adjust formatting of error/success text in Input component

--- src/components/input/Input.tsx
+++ src/components/input/Input.tsx
@@ -78,7 +78,9 @@ const Input = ({
           ) : (
             <CheckedCircleIcon className="w-3 h-3 md:w-4 md:h-4 fill-green-500" />
           )}
-          <span className="leading-none">{isError ? errorText : successText}</span>
+          <span className="leading-none">
+            {isError ? errorText : successText}
+          </span>
         </p>
       )}
     </div>
