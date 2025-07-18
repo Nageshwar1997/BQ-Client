6b3c7dfc9253632a8f089dedb1291e7fd89e3095
nageshwarp1997 <nageshwar@ctruh.com>
2025-07-18 17:23:01 +0530

    sortBy values updated

--- src/constants/index.ts
+++ src/constants/index.ts
@@ -2,12 +2,12 @@ export * from "./categories";
 export const DEFAULT_FILTER = { name: "All", value: "all", default: true };
 export const SORT_BY_OPTIONS = [
   { label: "Featured", value: "featured", disabled: false },
-  { label: "Best selling", value: "best-selling", disabled: true },
+  { label: "Best selling", value: "best-selling", disabled: false },
   { label: "Top rated", value: "top-rated", disabled: false },
   { label: "Alphabetically, A-Z", value: "a-z", disabled: false },
   { label: "Alphabetically, Z-A", value: "z-a", disabled: false },
-  { label: "Price, low to high", value: "price-low-high", disabled: false },
-  { label: "Price, high to low", value: "price-high-low", disabled: false },
-  { label: "Date, old to new", value: "date-old-new", disabled: false },
-  { label: "Date, new to old", value: "date-new-old", disabled: false },
+  { label: "Price, low to high", value: "price-asc", disabled: false },
+  { label: "Price, high to low", value: "price-desc", disabled: false },
+  { label: "Date, old to new", value: "old", disabled: false },
+  { label: "Date, new to old", value: "new", disabled: false },
 ];
