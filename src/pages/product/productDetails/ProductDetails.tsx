f3b1f4bf4330104ad638d19192cb1b17bc70c768
nageshwarp1997 <nageshwar@ctruh.com>
2025-07-20 22:15:00 +0530

    `Added product details page functionality, including fetching product data, populating fields, and rendering an image carousel.`

--- src/pages/product/productDetails/ProductDetails.tsx
+++ src/pages/product/productDetails/ProductDetails.tsx
@@ -1,5 +1,38 @@
+import { useMemo } from "react";
+import { useGetProductById } from "../../../api/product/product.service";
+import useQueryParams from "../../../hooks/useQueryParams";
+import { FetchedProductType } from "../../../types";
+import ImageCarousel from "../../../components/carousels/ImageCarousel";
+
 const ProductDetails = () => {
-  return <div>ProductDetails</div>;
+  const { params } = useQueryParams();
+  const productQuery = useGetProductById({
+    queryParams: { productId: params.productId as string },
+    data: {
+      populateFields: {
+        category: ["name"],
+        reviews: ["rating"],
+        shades: ["images", "colorCode", "stock", "shadeName"],
+      },
+    },
+  });
+
+  const product: FetchedProductType = productQuery.data?.product || {};
+  const images = useMemo(() => {
+    const commonImages = product?.commonImages ?? [];
+    const shadeImages = product?.shades?.map((shade) => shade?.images) ?? [];
+    return [...commonImages, ...shadeImages];
+  }, [product?.commonImages, product?.shades]);
+
+  return (
+    <div className="w-full h-auto p-5">
+      <div className="w-full lg:w-1/2">
+        {images.length && (
+          <ImageCarousel images={images as string[]} className="" />
+        )}
+      </div>
+    </div>
+  );
 };
 
 export default ProductDetails;
