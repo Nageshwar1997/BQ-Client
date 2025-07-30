4c0d352805c70d216c8680b3fbd27e121ab82617
nageshwarp1997 <nageshwar@ctruh.com>
2025-07-30 16:59:57 +0530

    created a carousel components for Media

---
+++ src/components/carousels/MediaCarousel.tsx
@@ -0,0 +1,74 @@
+import { LeftGradient, RightGradient } from "../Gradients";
+import useHorizontalScrollable from "../../hooks/useHorizontalScrollable";
+import { TMediaCarousel } from "../../types";
+import { PlayIcon } from "../../icons";
+import { convertVideoToPoster } from "../../utils";
+
+const MediaCarousel = ({
+  className = "",
+  data,
+  currentIndex,
+  setCurrentIndex,
+  thumbnailRefs,
+  onImageClick,
+}: TMediaCarousel) => {
+  const [showGradient, containerRef] = useHorizontalScrollable();
+
+  return (
+    <div className={`p-1 relative ${className}`}>
+      {showGradient.left && <LeftGradient className="!w-20 h-full" />}
+      <div
+        className={`flex items-center gap-2
+           ${
+             !showGradient.left && !showGradient.right
+               ? "justify-center"
+               : "overflow-x-scroll scroll-smooth overflow-hidden"
+           }
+        `}
+        ref={containerRef}
+      >
+        {data?.map((item, i) => (
+          <div
+            key={i}
+            ref={(el) => {
+              if (
+                thumbnailRefs?.current &&
+                !showGradient.left &&
+                !showGradient.right
+              ) {
+                thumbnailRefs.current[i] = el;
+              }
+            }}
+            onClick={() => {
+              setCurrentIndex(i);
+              onImageClick?.();
+            }}
+            className={`w-20 h-20 group rounded overflow-hidden border shadow-sm shrink-0 hover:opacity-100 transition-colors duration-300 ${
+              i === currentIndex
+                ? "border-tertiary opacity-100"
+                : "border-primary-30 opacity-90"
+            } ${item.type === "video" ? "relative" : ""}`}
+          >
+            {item.type === "video" && i !== currentIndex && (
+              <div className="absolute inset-0 w-full h-full aspect-square bg-black/50 flex items-center justify-center group pointer-events-none">
+                <PlayIcon className="fill-white opacity-90 group-hover:opacity-100" />
+              </div>
+            )}
+            <img
+              src={
+                item.type === "video"
+                  ? convertVideoToPoster(item.url)
+                  : item.url
+              }
+              alt={`image-${i}`}
+              className="w-full h-full object-cover cursor-pointer aspect-square"
+            />
+          </div>
+        ))}
+      </div>
+      {showGradient.right && <RightGradient className="!w-20 h-full" />}
+    </div>
+  );
+};
+
+export default MediaCarousel;
---
+++ src/components/carousels/MediaCarouselWithParentMedia.tsx
@@ -0,0 +1,125 @@
+import { useEffect, useMemo, useRef, useState } from "react";
+import { DropdownIcon } from "../../icons";
+import MediaCarousel from "./MediaCarousel";
+import VideoPlayer from "../videoPlayers/VideoPlayer";
+import { convertVideoToPoster } from "../../utils";
+import { IMediaCarouselWithParentMedia } from "../../types";
+
+const MediaCarouselWithParentMedia = ({
+  className,
+  data,
+  selected = 0,
+  needButtonControls = true,
+  videoProps,
+}: IMediaCarouselWithParentMedia) => {
+  const [currentIndex, setCurrentIndex] = useState(0);
+  const [mediaType, setMediaType] = useState<"image" | "video">("image");
+  const thumbnailRefs = useRef<(HTMLDivElement | null)[]>([]);
+
+  const src = useMemo(() => {
+    if (data[currentIndex]) {
+      return data[currentIndex].url;
+    }
+    return "";
+  }, [currentIndex, data]);
+
+  useEffect(() => {
+    if (selected === null || selected === undefined) return;
+    setCurrentIndex(selected); // Set the current index to the selected thumbnail
+    // Scroll the selected thumbnail into view
+    const el = thumbnailRefs.current[selected];
+    if (el) {
+      el.scrollIntoView({
+        behavior: "smooth",
+        inline: "center",
+        block: "nearest",
+      });
+    }
+  }, [selected]);
+
+  useEffect(() => {
+    if (data[currentIndex]) {
+      setMediaType(data[currentIndex].type);
+    }
+  }, [currentIndex, data]);
+
+  return (
+    <div
+      className={`bg-primary-inverted rounded-lg max-w-3xl w-full overflow-hidden ${className}`}
+    >
+      {/* Main Image */}
+      <div className="mb-4 h-[400px] lg:h-[420px] xl:h-[500px] flex items-center justify-center relative">
+        <div className="w-full h-full transform transition-opacity duration-500">
+          {mediaType === "video" ? (
+            <VideoPlayer
+              videoProps={{
+                ...videoProps,
+                src,
+                poster: convertVideoToPoster(src),
+                className: "object-contain",
+              }}
+              className="max-h-full mx-auto rounded-lg flex items-center justify-center"
+            />
+          ) : (
+            <img
+              src={src}
+              alt={`preview-${currentIndex}`}
+              className="max-h-full mx-auto object-contain rounded-lg"
+              loading="lazy"
+            />
+          )}
+        </div>
+
+        {needButtonControls && (
+          <div className="w-full py-2 absolute bottom-0 text-sm text-center flex items-center justify-center gap-5">
+            <button
+              type="button"
+              onClick={() => {
+                setCurrentIndex((prev) => {
+                  if (currentIndex > 0) {
+                    return prev - 1;
+                  } else {
+                    return data?.length - 1;
+                  }
+                });
+              }}
+              className="p-[5px] rounded border border-primary-50 bg-primary-inverted-50 cursor-pointer"
+            >
+              <DropdownIcon className="rotate-90 [&>path]:stroke-primary" />
+            </button>
+
+            <span className="py-2 w-24 px-4 min-h-full border border-primary-50 content-center bg-primary-inverted-50 text-primary leading-none rounded">
+              {currentIndex + 1} of {data.length}
+            </span>
+            <button
+              type="button"
+              onClick={() => {
+                setCurrentIndex((prev) => {
+                  if (currentIndex < data.length - 1) {
+                    return prev + 1;
+                  } else {
+                    return 0;
+                  }
+                });
+              }}
+              className="p-[5px] rounded border border-primary-50 bg-primary-inverted-50 cursor-pointer disabled:opacity-50 disabled:cursor-not-allowed"
+            >
+              <DropdownIcon className="-rotate-90 [&>path]:stroke-primary" />
+            </button>
+          </div>
+        )}
+      </div>
+
+      <hr className="h-px mb-4 block border-none bg-gradient-line" />
+      {/* Thumbnails */}
+      <MediaCarousel
+        data={data}
+        currentIndex={currentIndex}
+        setCurrentIndex={setCurrentIndex}
+        thumbnailRefs={thumbnailRefs}
+      />
+    </div>
+  );
+};
+
+export default MediaCarouselWithParentMedia;
