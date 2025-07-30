02669b29a4f21f8b515dad97bdb5d95565f3240d
nageshwarp1997 <nageshwar@ctruh.com>
2025-07-30 16:41:14 +0530

    VideoPlayer Updated

--- src/components/videoPlayers/VideoPlayer.tsx
+++ src/components/videoPlayers/VideoPlayer.tsx
@@ -1,49 +1,31 @@
 import { useEffect, useRef } from "react";
 import Hls from "hls.js";
+import { IVideoPlayer } from "../../types";
 
-interface VideoPlayerProps {
-  videoUrl: string;
-  posterUrl?: string;
-  autoPlay?: boolean;
-  controls?: boolean;
-  isMuted?: boolean;
-  width?: string | number;
-  height?: string | number;
-  className?: string;
-}
-
-const VideoPlayer = ({
-  videoUrl,
-  posterUrl = "",
-  controls = false,
-  autoPlay = true,
-  className = "",
-  isMuted = false,
-}: VideoPlayerProps) => {
+const VideoPlayer = ({ className = "", videoProps = {} }: IVideoPlayer) => {
   const videoRef = useRef<HTMLVideoElement | null>(null);
 
   useEffect(() => {
     const video = videoRef.current;
     let hls: Hls | null = null;
-    if (!video) return;
-    video.muted = isMuted;
+    if (!video || !videoProps.src) return;
 
     if (Hls.isSupported()) {
       hls = new Hls();
-      hls.loadSource(videoUrl);
+      hls.loadSource(videoProps.src);
       hls.attachMedia(video);
 
       hls.on(Hls.Events.MANIFEST_PARSED, () => {
-        if (autoPlay) {
+        if (videoProps.autoPlay) {
           video
             .play()
             .catch((err) => console.warn("Autoplay blocked (HLS):", err));
         }
       });
     } else if (video.canPlayType("application/vnd.apple.mpegurl")) {
-      video.src = videoUrl;
+      video.src = videoProps.src;
       video.addEventListener("loadedmetadata", () => {
-        if (autoPlay) {
+        if (videoProps.autoPlay) {
           video
             .play()
             .catch((err) => console.warn("Autoplay blocked (native):", err));
@@ -54,17 +36,21 @@ const VideoPlayer = ({
     return () => {
       if (hls) hls.destroy();
     };
-  }, [videoUrl, autoPlay, isMuted]);
+  }, [videoProps]);
 
   return (
-    <div className={`relative w-full h-full aspect-video ${className}`}>
+    <div className={`w-full h-full ${className}`}>
       <video
         ref={videoRef}
-        controls={controls}
-        muted={isMuted}
-        className="object-cover w-full h-full"
-        poster={posterUrl}
-        playsInline
+        {...videoProps}
+        playsInline={videoProps.playsInline ?? true}
+        autoPlay={videoProps.autoPlay ?? true}
+        muted={videoProps.muted ?? true}
+        loop={videoProps.loop ?? false}
+        controls={videoProps.controls ?? false}
+        className={`object-cover aspect-auto w-full h-full ${
+          videoProps.className ?? ""
+        }`}
       />
     </div>
   );
