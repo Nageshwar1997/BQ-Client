fd19bb7402725a18493ebbf51fbae3eeb33c30fb
nageshwarp1997 <nageshwar@ctruh.com>
2025-08-02 01:33:08 +0530

    Dropdown component updated and optimized

--- src/components/dropdown/Dropdown.tsx
+++ src/components/dropdown/Dropdown.tsx
@@ -1,62 +1,81 @@
-import { useEffect, useRef, useState } from "react";
+import {
+  cloneElement,
+  isValidElement,
+  useEffect,
+  useRef,
+  useState,
+} from "react";
 import { DropdownIcon } from "../../icons";
 import { TDropdown } from "../../types";
+import useOutsideClick from "../../hooks/useOutsideClick";
 
 const Dropdown = ({
   title,
   icons,
   children,
   className = "",
-  options = [],
+  closeOnOptionClick = false,
+  closeOnOutsideClick = false,
+  isAbsolute = false,
+  showShadow = false,
 }: TDropdown) => {
-  const [isOpen, setIsOpen] = useState(false);
-  const contentRef = useRef<HTMLDivElement>(null);
-  const [height, setHeight] = useState<number>(0);
+  const [isOpen, setIsOpen] = useState<boolean>(false);
+  const containerRef = useRef<HTMLDivElement | null>(null);
+  const ref = useOutsideClick<HTMLDivElement>(
+    () => (isOpen ? setIsOpen(false) : null),
+    { enabled: closeOnOutsideClick }
+  );
 
   useEffect(() => {
-    if (contentRef.current) {
-      setHeight(isOpen ? contentRef.current.scrollHeight : 0);
+    if (!containerRef.current) return;
+    if (isOpen) {
+      containerRef.current.style.maxHeight = `${containerRef.current.scrollHeight}px`;
+      containerRef.current.style.opacity = "1";
+    } else {
+      containerRef.current.style.maxHeight = "0px";
+      containerRef.current.style.opacity = "0";
     }
-  }, [isOpen, options]);
+  }, [isOpen]);
   return (
-    <div className={`px-4 ${className}`}>
-      <div className="border-b border-b-primary-50">
-        <button
-          onClick={() => setIsOpen((prev) => !prev)}
-          className="flex items-center justify-between gap-2 w-full px-1 pt-3 pb-5 text-left transition-colors duration-300 group"
-          aria-expanded={isOpen}
-        >
-          <div className="flex items-center gap-2">
-            {icons?.left && (
-              <span className="transition-colors flex items-center justify-center">
-                {icons.left}
-              </span>
-            )}
-            <span className="text-sm sm:text-[15px] text-primary font-medium whitespace-nowrap">
-              {title}
-            </span>
-            {icons?.right && (
-              <span className="transition-colors flex items-center justify-center">
-                {icons.right}
-              </span>
-            )}
-          </div>
-          <DropdownIcon
-            className={`w-5 h-5 transition-transform duration-300 ease-in-out stroke-primary ${
-              isOpen ? "rotate-180" : "rotate-0"
-            }`}
-          />
-        </button>
-        <div
-          style={{ height: `${height}px` }}
-          className={`overflow-hidden transition-all duration-300 ease-in-out ${
-            isOpen ? "mb-2" : ""
-          }`}
-        >
-          <div ref={contentRef} className="w-full h-fit">
-            {children}
-          </div>
+    <div
+      className={`w-full flex flex-col items-start cursor-pointer ${
+        isAbsolute ? "relative" : "static"
+      } ${className}`}
+      ref={ref}
+    >
+      <button
+        className="flex items-center justify-between gap-2 w-full p-3 text-left transition-colors duration-300 group"
+        onClick={() => setIsOpen((prev) => !prev)}
+        aria-expanded={isOpen}
+      >
+        <div className="flex items-center gap-2 [&>span]:transition-all [&>span]:duration-300 [&>span]:flex [&>span]:items-center [&>span]:justify-center [&>span]:text-sm [&>span]:sm:text-base [&>span]:text-primary">
+          {icons?.left && <span>{icons.left}</span>}
+          <span>{title}</span>
+          {icons?.right && <span>{icons.right}</span>}
         </div>
+        <DropdownIcon
+          className={`w-5 h-5 transition-transform duration-300 ease-in-out stroke-primary ${
+            isOpen ? "rotate-180" : "rotate-0"
+          }`}
+        />
+      </button>
+      <div
+        ref={containerRef}
+        className={`transition-all duration-500 ease-in-out overflow-hidden opacity-0 w-full max-h-0 bg-primary-inverted ${
+          isAbsolute ? "absolute top-full inset-x-0 mt-2 z-10" : ""
+        } ${
+          showShadow
+            ? "shadow-[var(--primary-8)_0px_4px_16px,_var(--primary-10)_0px_8px_32px]"
+            : ""
+        }`}
+      >
+        {isValidElement(children)
+          ? cloneElement(children, {
+              onSelect: () => {
+                if (closeOnOptionClick) setIsOpen(false);
+              },
+            })
+          : children}
       </div>
     </div>
   );
