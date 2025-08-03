05495e0f63da8dea0a249da957428ffb73c0b8f0
nageshwarp1997 <nageshwar@ctruh.com>
2025-08-03 12:29:54 +0530

    `Refactor CustomerReviews component layout and styling`

--- src/pages/product/productDetails/CustomerReviews.tsx
+++ src/pages/product/productDetails/CustomerReviews.tsx
@@ -13,19 +13,19 @@ const CustomerReviews = ({ reviews }: { reviews: number[] }) => {
   });
 
   return (
-    <div className="space-y-3 p-5">
+    <div className="flex flex-col gap-1 md:gap-3 p-5">
       {Object.entries(ratingCounts)
         ?.sort((a, b) => Number(b[0]) - Number(a[0])) // Optional: ensure descending order
         ?.map(([rating, count]) => {
           const percent = reviews?.length ? (count / reviews?.length) * 100 : 0;
 
           return (
-            <div key={rating} className="flex items-center gap-5">
+            <div key={rating} className="flex items-center gap-3 md:gap-5">
               <RatingStars
                 rating={Number(rating)}
                 className="[&>svg]:w-4 [&>svg]:h-4 md:[&>svg]:w-5 md:[&>svg]:h-5 gap-0 md:gap-0.5"
               />
-              <div className="relative h-5 bg-primary-10 rounded-sm w-full max-w-xs">
+              <div className="relative h-4 md:h-5 bg-primary-10 rounded-sm w-full max-w-xs">
                 <div
                   className="absolute top-0 left-0 h-full bg-primary rounded-sm"
                   style={{ width: `${percent}%` }}
