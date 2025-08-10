36ac5f024a3e532a9cd587161b58226a33ed0baf
nageshwarp1997 <nageshwar@ctruh.com>
2025-08-10 20:29:06 +0530

    `Refactored ProductDetailsSkeleton, ProductReviews, and ReviewsSection components with improved loading states and error handling.`

--- src/pages/product/productDetails/children/ProductReviews.tsx
+++ src/pages/product/productDetails/children/ProductReviews.tsx
@@ -1,14 +1,23 @@
-import { useState } from "react";
+import { Fragment, useState } from "react";
 import { FetchedReviewType, TCarouselOption } from "../../../../types";
 import ReviewMediaModal from "./ReviewMediaModal";
 import ReviewCard from "./ReviewCard";
+import ShowError from "../../../../components/errors/ShowError";
+import EmptyData from "../../../../components/empty-data/EmptyData";
+import ReviewCardSkeleton from "../../../../components/skeletons/children/ReviewCardSkeleton";
 
 const ProductReviews = ({
   reviews,
+  isLoading,
+  isError,
+  ref,
+  isFetchingNextPage = false,
 }: {
   reviews: FetchedReviewType[];
   isLoading: boolean;
   isError: boolean;
+  ref: (node?: Element | null | undefined) => void;
+  isFetchingNextPage?: boolean;
 }) => {
   const [mediaIndex, setMediaIndex] = useState<null | number>(null);
   const [selectedReviewMedia, setSelectedReviewMedia] = useState<
@@ -17,21 +26,42 @@ const ProductReviews = ({
 
   return (
     <div className="flex flex-col gap-4">
-      {reviews?.map((review, index) => {
-        return (
-          <ReviewCard
-            key={index}
-            className={`${index === 0 ? "pt-4" : ""} ${
-              index !== reviews.length - 1 ? "border-b border-b-primary-10" : ""
-            }`}
-            review={review}
-            onMediaClick={(reviewMedia, index) => {
-              setSelectedReviewMedia(reviewMedia);
-              setMediaIndex(index);
-            }}
-          />
-        );
-      })}
+      {isLoading ? (
+        <ReviewCardSkeleton className="pt-4" />
+      ) : isError ? (
+        <ShowError
+          headingText="Something went wrong!"
+          descriptionText="Failed to get the reviews. Please reload the page"
+          className="w-full mx-auto mb-auto [&>h3]:text-base [&>h3]:base:text-base [&>h3]:sm:text-xl [&>h3]:md:text-2xl [&>h3]:lg:text-3xl [&>h3]:xl:text-4xl [&>h3]:uppercase [&>p]:text-xs [&>p]:base:text-sm [&>p]:sm:text-base [&>p]:md:text-lg"
+        />
+      ) : reviews.length > 0 ? (
+        <Fragment>
+          {reviews.map((review, index) => (
+            <div key={index} ref={index === reviews.length - 1 ? ref : null}>
+              <ReviewCard
+                className={`${index === 0 ? "pt-4" : ""} ${
+                  index !== reviews.length - 1
+                    ? "border-b border-b-primary-10"
+                    : ""
+                }`}
+                review={review}
+                onMediaClick={(reviewMedia, index) => {
+                  setSelectedReviewMedia(reviewMedia);
+                  setMediaIndex(index);
+                }}
+              />
+            </div>
+          ))}
+          {isFetchingNextPage && (
+            <ReviewCardSkeleton className="border-t border-t-primary-10 pt-4" />
+          )}
+        </Fragment>
+      ) : (
+        <EmptyData
+          content={"Reviews not available"}
+          className="w-full mx-auto [&>h3]:text-base [&>h3]:base:text-base [&>h3]:sm:text-xl [&>h3]:md:text-2xl [&>h3]:lg:text-3xl [&>h3]:xl:text-4xl [&>h3]:uppercase gap-5"
+        />
+      )}
       {selectedReviewMedia.length > 0 && (
         <ReviewMediaModal
           currentIndex={mediaIndex}
--- src/pages/product/productDetails/children/ReviewsSection.tsx
+++ src/pages/product/productDetails/children/ReviewsSection.tsx
@@ -1,4 +1,4 @@
-import { useMemo } from "react";
+import { useEffect, useMemo } from "react";
 import { FetchedReviewType } from "../../../../types";
 import ProductReviews from "./ProductReviews";
 import Dropdown from "../../../../components/dropdown/Dropdown";
@@ -7,28 +7,39 @@ import { REVIEWS_OPTIONS } from "../../../../constants";
 import DropdownOptions from "../../../../components/dropdown/children/DropdownOptions";
 import { useGetAllReviewsByProductIdInfinite } from "../../../../api/reviews/reviews.service";
 import ShowError from "../../../../components/errors/ShowError";
-// import { useInView } from "react-intersection-observer";
+import { useInView } from "react-intersection-observer";
 
 const ReviewsSection = () => {
   const { params, queryParams, setParams, removeParam } = useQueryParams();
-  // const { ref, inView } = useInView();
+  const { ref, inView } = useInView();
 
-  const { data, isError, isLoading } = useGetAllReviewsByProductIdInfinite({
-    pageParams: { limit: 5 },
+  const {
+    data,
+    isError,
+    isLoading,
+    hasNextPage,
+    fetchNextPage,
+    isFetchingNextPage,
+  } = useGetAllReviewsByProductIdInfinite({
+    pageParams: { limit: 2 },
     queryParams: {
       productId: params.productId || "",
       sortBy: queryParams.sortBy,
     },
     data: {
-      populateFields: {
-        user: ["firstName", "lastName", "profilePic"],
-      },
+      populateFields: { user: ["firstName", "lastName", "profilePic"] },
     },
   });
 
   const reviews: FetchedReviewType[] = useMemo(() => {
     return data?.pages.flatMap((page) => page.reviews) || [];
   }, [data]);
+
+  useEffect(() => {
+    if (inView && hasNextPage) {
+      fetchNextPage();
+    }
+  }, [inView, hasNextPage, fetchNextPage]);
 
   return (
     <div className="flex flex-col">
@@ -39,42 +50,42 @@ const ReviewsSection = () => {
           className="w-full mx-auto mb-auto [&>h3]:text-base [&>h3]:base:text-base [&>h3]:sm:text-xl [&>h3]:md:text-2xl [&>h3]:lg:text-3xl [&>h3]:xl:text-4xl [&>h3]:uppercase [&>p]:text-xs [&>p]:base:text-sm [&>p]:sm:text-base [&>p]:md:text-lg"
         />
       ) : null}
-      {reviews.length > 0 && (
-        <div className="w-full py-2 border-y border-y-primary-50 space-y-4 relative">
-          <Dropdown
-            title={
-              REVIEWS_OPTIONS.find((o) => o.value === queryParams?.sortBy)
-                ?.name ?? REVIEWS_OPTIONS[0].name
-            }
-            isAbsolute={true}
-            closeOnOptionClick={true}
-            closeOnOutsideClick={true}
-            showShadow={true}
-            className="!w-[180px] ml-auto [&>button]:border [&>button]:border-primary-30 [&>button]:rounded-md [&>div]:rounded-md sticky top-[65px]"
-          >
-            <DropdownOptions
-              options={REVIEWS_OPTIONS}
-              selected={queryParams?.sortBy ?? REVIEWS_OPTIONS[0].value}
-              onChange={(data) => {
-                if (
-                  queryParams?.sortBy === data.value ||
-                  data.value === "most-recent"
-                ) {
-                  removeParam("sortBy");
-                } else {
-                  setParams({ sortBy: data.value });
-                }
-              }}
-              className="[&>button]:text-nowrap"
-            />
-          </Dropdown>
-          <ProductReviews
-            reviews={reviews}
-            isError={isError}
-            isLoading={isLoading}
+      <div className="w-full py-2 border-y border-y-primary-50 space-y-4 relative">
+        <Dropdown
+          title={
+            REVIEWS_OPTIONS.find((o) => o.value === queryParams?.sortBy)
+              ?.name ?? REVIEWS_OPTIONS[0].name
+          }
+          isAbsolute={true}
+          closeOnOptionClick={true}
+          closeOnOutsideClick={true}
+          showShadow={true}
+          className="!w-[180px] ml-auto [&>button]:border [&>button]:border-primary-30 [&>button]:rounded-md [&>div]:rounded-md sticky top-[65px]"
+        >
+          <DropdownOptions
+            options={REVIEWS_OPTIONS}
+            selected={queryParams?.sortBy ?? REVIEWS_OPTIONS[0].value}
+            onChange={(data) => {
+              if (
+                queryParams?.sortBy === data.value ||
+                data.value === "most-recent"
+              ) {
+                removeParam("sortBy");
+              } else {
+                setParams({ sortBy: data.value });
+              }
+            }}
+            className="[&>button]:text-nowrap bg-secondary-inverted"
           />
-        </div>
-      )}
+        </Dropdown>
+        <ProductReviews
+          reviews={reviews}
+          isError={isError}
+          isLoading={isLoading}
+          ref={ref}
+          isFetchingNextPage={isFetchingNextPage}
+        />
+      </div>
     </div>
   );
 };
