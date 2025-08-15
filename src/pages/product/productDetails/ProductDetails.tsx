15b8ba03215d84d604714dfe1deec3ebb95333e3
nageshwarp1997 <nageshwar@ctruh.com>
2025-08-15 18:46:46 +0530

    feat: replace customer reviews section with RatingBars component for improved review display

--- src/pages/product/productDetails/ProductDetails.tsx
+++ src/pages/product/productDetails/ProductDetails.tsx
@@ -15,8 +15,7 @@ import ProductVariants from "./children/ProductVariants";
 import ProductDetailsSkeleton from "../../../components/skeletons/children/ProductDetailsSkeleton";
 import ShowError from "../../../components/errors/ShowError";
 import EmptyData from "../../../components/empty-data/EmptyData";
-import CustomerReviews from "./CustomerReviews";
-import TextDisplay from "../../../components/TextDisplay";
+import RatingBars from "./children/RatingBars";
 import ReviewsMedia from "./children/ReviewsMedia";
 
 const ProductDetails = () => {
@@ -153,37 +152,7 @@ const ProductDetails = () => {
         )}
       </div>
       <SimilarProducts category={product.category} />
-      <div className="w-full pb-8 border-b border-b-primary-50 space-y-4">
-        <TextDisplay
-          content={[{ isHighlighted: true, text: "Customer Reviews" }]}
-          className="text-xl md:text-3xl lg:text-4xl"
-        />
-        <hr className="max-w-xl mx-auto h-px block border-none bg-gradient-line my-4" />
-        <div className="max-w-4xl mx-auto flex flex-col sm:flex-row gap-8">
-          <div className="w-full">
-            <CustomerReviews reviews={product?.reviews?.map((r) => r.rating)} />
-          </div>
-          <div className="w-full flex flex-col gap-4 items-center justify-center">
-            <Button
-              pattern="secondary"
-              content="Write a Review"
-              className="max-w-44 py-2! lg:py-3 !rounded-md"
-            />
-            <div className="flex flex-col gap-0.5 items-center justify-center text-secondary">
-              <div className="flex items-center gap-2 text-sm">
-                <RatingStars
-                  rating={getAvgRating(product.reviews)}
-                  className="[&>svg]:w-4 [&>svg]:h-4"
-                />
-                <span className="leading-none mt-px">
-                  {getAvgRating(product.reviews)} out of 5
-                </span>
-              </div>
-              <p>Based on {product?.reviews?.length} reviews</p>
-            </div>
-          </div>
-        </div>
-      </div>
+      <RatingBars reviews={product?.reviews} />
       <ReviewsMedia
         reviews={product?.reviews}
         isError={isError}
---
+++ src/pages/product/productDetails/children/RatingBars.tsx
@@ -0,0 +1,56 @@
+import { useState } from "react";
+import Button from "../../../../components/button/Button";
+import RatingStars from "../../../../components/navbar/components/rating/RatingStars";
+import TextDisplay from "../../../../components/TextDisplay";
+import { FetchedReviewType } from "../../../../types";
+import { getAvgRating } from "../../../../utils";
+import CustomerReviews from "../CustomerReviews";
+import AddReviewModal from "../../../../components/modal/children/AddReviewModal";
+
+const RatingBars = ({ reviews = [] }: { reviews: FetchedReviewType[] }) => {
+  const [showAddReviewModal, setShowAddReviewModal] = useState(false);
+  return (
+    <>
+      <div className="w-full pb-8 border-b border-b-primary-50 space-y-4">
+        <TextDisplay
+          content={[{ isHighlighted: true, text: "Customer Reviews" }]}
+          className="text-xl md:text-3xl lg:text-4xl"
+        />
+        <hr className="max-w-xl mx-auto h-px block border-none bg-gradient-line my-4" />
+        <div className="max-w-4xl mx-auto flex flex-col sm:flex-row gap-8">
+          <div className="w-full">
+            <CustomerReviews reviews={reviews?.map((r) => r.rating)} />
+          </div>
+          <div className="w-full flex flex-col gap-4 items-center justify-center">
+            <Button
+              pattern="secondary"
+              content="Write a Review"
+              className="max-w-44 py-2! lg:py-3 !rounded-md"
+              onClick={() => setShowAddReviewModal(true)}
+            />
+            <div className="flex flex-col gap-0.5 items-center justify-center text-secondary">
+              <div className="flex items-center gap-2 text-sm">
+                <RatingStars
+                  rating={getAvgRating(reviews)}
+                  className="[&>svg]:w-4 [&>svg]:h-4"
+                />
+                <span className="leading-none mt-px">
+                  {getAvgRating(reviews)} out of 5
+                </span>
+              </div>
+              <p>Based on {reviews?.length} reviews</p>
+            </div>
+          </div>
+        </div>
+      </div>
+      {showAddReviewModal && (
+        <AddReviewModal
+          isOpen={showAddReviewModal}
+          onClose={() => setShowAddReviewModal(false)}
+        />
+      )}
+    </>
+  );
+};
+
+export default RatingBars;
