5cefdb638e7334b60056d3c991f5970240076a99
nageshwarp1997 <nageshwar@ctruh.com>
2025-08-15 18:45:14 +0530

    feat: add regex validation constants to improve input validation

--- src/constants/index.ts
+++ src/constants/index.ts
@@ -1,3 +1,5 @@
+import { TRegexes } from "../types";
+
 export * from "./categories";
 export const DEFAULT_FILTER = { name: "All", value: "all", default: true };
 export const SORT_BY_OPTIONS = [
@@ -40,6 +42,39 @@ export const CATEGORY_VIDEOS = [
   },
 ];
 
-// 67d6c05450e27d041247edbc
-// 681a654ce69db60521464810
-// 6824cff10f64d02b9cfd9bca
+export const MB = 1024 ** 2;
+export const MAX_IMAGE_FILE_SIZE = 1 * MB; // 2MB
+export const MAX_VIDEO_FILE_SIZE = 1 * MB; // 50MB
+
+export const ALLOWED_IMAGE_TYPES = [
+  "image/jpeg",
+  "image/png",
+  "image/webp",
+  "image/jpg",
+];
+
+export const ALLOWED_VIDEO_TYPES = ["video/mp4", "video/webm"];
+
+export const regexes: Record<TRegexes, RegExp> = {
+  noSpace: /^\S+$/, // No spaces allowed
+  singleSpace: /^(?!.*\s{2,}).*$/, // Single space allowed
+  hexCode: /^#(?:[0-9a-fA-F]{3}|[0-9a-fA-F]{4}|[0-9a-fA-F]{6}|[0-9a-fA-F]{8})$/, // Hex color code
+  date: /^\d{4}-\d{2}-\d{2}(T\d{2}:\d{2}:\d{2}(?:\.\d+)?(Z|([+-]\d{2}:\d{2}))?)?$/, // Date e.g. 2022-01-01T12:00:00Z
+  name: /^(?!.*\d)(?!.* {2})([A-Za-z]+( [A-Za-z]+)*)$/, // Only letters & single space
+  password: /^(?=.*[A-Z])(?=.*[a-z])(?=.*\d)(?=.*[@$!%*?&#])(?=\S.*$).{6,20}$/, // Password must contain at least one uppercase letter, one lowercase letter, one number, one special character, and be at least 6 characters long
+  email:
+    /^[a-zA-Z0-9]+([._%+-]?[a-zA-Z0-9]+)*@[a-zA-Z0-9]+(-?[a-zA-Z0-9]+)*(\.[a-zA-Z]{2,})+$/, // Email e.g. 3oYQK@example.com
+  phoneStart: /^[6-9]/, // Starts with 6, 7, 8, or 9
+  phoneExactLength: /^\d{10}$/, // Exactly 10 digits
+  phone: /^[6-9][0-9]{9}$/, // Phone number e.g. 9876543210
+  atLeastOneUppercaseLetter: /[A-Z]/, // At least one uppercase letter
+  atLeastOneLowercaseLetter: /[a-z]/, // At least one lowercase letter
+  atLeastOneDigit: /\d/, // At least one digit
+  atLeastOneSpecialCharacter: /[@$!%*?&#]/, // At least one special character
+  onlyDigits: /^\d+$/, // All characters are digits
+  onlyUppercase: /^[A-Z]+$/, // All characters are uppercase
+  onlyLowercase: /^[a-z]+$/, // All characters are lowercase
+  onlyLetters: /^[a-zA-Z]+$/, // All characters are letters
+  onlyLettersAndSpaces: /^[a-zA-Z\s]+$/, // All characters are letters and spaces
+  onlyLettersAndSpacesAndDots: /^[a-zA-Z\s.]+$/, // Only letters, spaces, and dots
+};
