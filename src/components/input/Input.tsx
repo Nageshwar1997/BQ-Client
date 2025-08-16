01228adce3193743dc33d1e79ad66ae7a6d8309a
nageshwarp1997 <nageshwar@ctruh.com>
2025-08-16 20:20:37 +0530

    refactor: streamline Input component by consolidating props and enhancing icon handling

--- src/components/input/Input.tsx
+++ src/components/input/Input.tsx
@@ -1,44 +1,27 @@
 import { ChangeEvent } from "react";
-import { CheckedCircleIcon, InfoIcon } from "../../icons";
-import { InputProps } from "../../types";
+import { InfoIcon } from "../../icons";
+import { IInput } from "../../types";
 
 const Input = ({
-  min,
-  max,
-  value,
-  name = "",
   label = "",
   register,
-  leftIcon,
-  rightIcon,
-  onChange,
-  onKeyDown,
-  leftIconClick,
-  rightIconClick,
-  type = "text",
   className = "",
-  errorText = "",
-  readOnly = false,
-  successText = "",
-  placeholder = "",
-  autoComplete = "off",
+  error = "",
   containerClassName = "",
-  leftText = "",
-}: InputProps) => {
+  icons,
+  inputProps,
+}: IInput) => {
   const handleChange = (event: ChangeEvent<HTMLInputElement>) => {
-    onChange?.(event);
+    inputProps?.onChange?.(event);
     register?.onChange?.(event);
   };
-
-  const isError = errorText && !successText;
-  const isSuccess = successText && !errorText;
 
   return (
     <div className={`w-full flex flex-col gap-1.5 ${containerClassName}`}>
       <div className="relative h-10 lg:h-12">
         {label && (
           <label
-            htmlFor={name}
+            htmlFor={inputProps.name}
             className={`text-[10px] lg:text-xs text-primary-50 absolute top-0 left-3 transform -translate-y-1/2 border border-primary-10 leading-none px-1 md:px-2 py-0.5 bg-smoke-eerie rounded cursor-pointer`}
           >
             {label}
@@ -48,71 +31,63 @@ const Input = ({
           className={`w-full h-full flex items-center gap-1 border border-primary-10 bg-smoke-eerie rounded-lg overflow-hidden ${className}`}
         >
           {/* Left Icon */}
-          {leftIcon && !rightIcon ? (
+          {icons?.left?.icon && !icons.right?.icon ? (
             <span
-              onClick={leftIconClick && leftIconClick}
+              onClick={icons.left.onClick}
               className="h-full flex justify-center items-center cursor-pointer p-2 overflow-hidden"
             >
-              {leftIcon}
+              {icons.left.icon}
             </span>
-          ) : !leftIcon && leftText ? (
+          ) : !icons?.left?.icon && icons?.left?.text ? (
             <div className="h-full overflow-hidden">
               <p className="h-full flex items-center justify-center text-sm text-primary-50 border-r border-r-primary-10 p-3 capitalize">
-                {leftText}
+                {icons?.left?.text}
               </p>
             </div>
           ) : null}
           {/* Input */}
           <input
             aria-autocomplete="none"
-            min={min}
-            max={max}
-            id={name}
-            type={type}
-            name={name}
-            value={value}
             {...register}
-            readOnly={readOnly}
-            disabled={readOnly}
-            onKeyDown={onKeyDown}
+            {...inputProps}
+            id={inputProps.name}
+            disabled={inputProps?.readOnly}
             onChange={handleChange}
-            placeholder={placeholder}
-            autoComplete={autoComplete}
-            onWheel={(event) => type === "number" && event.currentTarget.blur()}
+            onWheel={(event) =>
+              inputProps?.type === "number" ? event.currentTarget.blur() : null
+            }
             className={`flex-1 w-full h-full outline-none border-none focus:outline-none focus:border-none bg-transparent font-normal text-sm p-3 text-primary placeholder:text-primary-50 placeholder:text-sm autofill-effect line-clamp-1 ${
-              leftIcon && !rightIcon
+              icons?.left?.icon && !icons?.right?.icon
                 ? "pl-0"
-                : !leftIcon && rightIcon
+                : !icons?.left?.icon && icons?.right?.icon
                 ? "pr-0"
-                : leftText
+                : icons?.left?.text
                 ? "pl-2"
                 : ""
-            } ${type === "number" ? "number-input-mouse-control-none" : ""}`}
+            } ${
+              inputProps?.type === "number"
+                ? "number-input-mouse-control-none"
+                : ""
+            }`}
           />
           {/* Right Icon */}
-          {!leftIcon && rightIcon && (
+          {!icons?.left && icons?.right && (
             <span
-              onClick={rightIconClick && rightIconClick}
+              onClick={icons.right.onClick}
               className="h-full flex justify-center items-center cursor-pointer p-2 overflow-hidden"
             >
-              {rightIcon}
+              {icons.right.icon}
             </span>
           )}
         </div>
       </div>
 
-      {!readOnly && (isError || isSuccess) && (
+      {!inputProps?.readOnly && error && (
         <p
           className={`w-full text-start flex gap-1 items-center text-[11px] leading-tight text-red-500`}
         >
-          {isError ? (
-            <InfoIcon className="w-3 h-3 md:w-4 md:h-4 fill-red-500" />
-          ) : (
-            <CheckedCircleIcon className="w-3 h-3 md:w-4 md:h-4 fill-green-500" />
-          )}
-          <span className="leading-none">
-            {isError ? errorText : successText}
-          </span>
+          <InfoIcon className="w-3 h-3 md:w-4 md:h-4 fill-red-500" />
+          <span className="leading-none">{error}</span>
         </p>
       )}
     </div>
