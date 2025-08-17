f947908da5573e69dc9f0ad5d8d439e8bcab2543
nageshwarp1997 <nageshwar@ctruh.com>
2025-08-17 11:59:11 +0530

    refactor: enhance addReviewSchema to include rating validation and media file type checks

--- src/schemas/review.ts
+++ src/schemas/review.ts
@@ -1,7 +1,9 @@
 import z from "zod";
 import { zodFileOrUrl, zodStringRequired } from "../utils/zod";
+import { ALLOWED_IMAGE_TYPES, ALLOWED_VIDEO_TYPES } from "../constants";
 
 export const addReviewSchema = z.object({
+  rating: z.coerce.number().min(1).max(5),
   title: zodStringRequired({
     field: "title",
     showingFieldName: "Title",
@@ -15,11 +17,35 @@ export const addReviewSchema = z.object({
     blockMultipleSpaces: true,
   }),
   media: z
-    .array(z.instanceof(File), z.string())
+    .array(z.union([z.instanceof(File), z.string()]))
     .superRefine((files, ctx) => {
-      files.forEach((file, index) =>
-        zodFileOrUrl({ fileOrUrl: file, field: "media", ctx, index })
-      );
+      let images = 0;
+      let videos = 0;
+
+      files.forEach((file) => {
+        if (file instanceof File) {
+          if (ALLOWED_IMAGE_TYPES.includes(file.type)) {
+            images++;
+          } else if (ALLOWED_VIDEO_TYPES.includes(file.type)) {
+            videos++;
+          }
+        }
+      });
+      if (images > 5) {
+        ctx.addIssue({
+          code: z.ZodIssueCode.custom,
+          message: "You can upload a maximum of 5 images",
+        });
+      } else if (videos > 5) {
+        ctx.addIssue({
+          code: z.ZodIssueCode.custom,
+          message: "You can upload a maximum of 5 videos",
+        });
+      } else {
+        files.forEach((file, index) => {
+          zodFileOrUrl({ fileOrUrl: file, field: "media", ctx, index });
+        });
+      }
     })
     .optional()
     .default([]),
