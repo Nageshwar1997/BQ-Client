61d66c8497442d4e32c1b75f4f72df443f4cbb4c
nageshwarp1997 <nageshwar@ctruh.com>
2025-08-17 11:58:29 +0530

    refactor: enhance add_review function for improved error handling and response logging

--- src/api/reviews/reviews.api.ts
+++ src/api/reviews/reviews.api.ts
@@ -2,6 +2,32 @@ import { AxiosError } from "axios";
 import api from "../../configs/axios.instance.config";
 import { IReviewsApiParams } from "../types";
 import { reviewRoutes } from "../api.routes";
+import { getUserToken } from "../../utils";
+
+export const add_review = async (data: FormData, productId: string) => {
+  const user_token = getUserToken();
+
+  try {
+    const { method, url } = reviewRoutes.addReview;
+    const response = await api.request({
+      method,
+      url: `${url}/${productId}`,
+      data,
+      headers: {
+        Authorization: user_token,
+        "Content-Type": "multipart/form-data",
+      },
+    });
+
+    console.log("response", response.data);
+    return response.data;
+  } catch (error) {
+    if (error instanceof AxiosError) {
+      throw error?.response?.data?.message || "API Error occurred";
+    }
+    throw "Something went wrong!";
+  }
+};
 
 export const get_reviews_by_product_id = async ({
   pageParams,
