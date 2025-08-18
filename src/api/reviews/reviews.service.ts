ff32c71d1fa8df2cc6d18e7f9e2d11a31eb8a277
nageshwarp1997 <nageshwar@ctruh.com>
2025-08-18 18:15:58 +0530

    `feat: updated reviews.service.ts to use new @tanstack/react-query features`

--- src/api/reviews/reviews.service.ts
+++ src/api/reviews/reviews.service.ts
@@ -1,4 +1,10 @@
-import { useInfiniteQuery, useMutation, useQuery } from "@tanstack/react-query";
+import {
+  keepPreviousData,
+  useInfiniteQuery,
+  useMutation,
+  useQuery,
+  useQueryClient,
+} from "@tanstack/react-query";
 import {
   add_review,
   get_reviews_by_product_id,
@@ -11,14 +17,24 @@ import {
 } from "../types";
 import useQueryParams from "../../hooks/useQueryParams";
 
+// Add Review
 export const useAddReview = () => {
   const { params } = useQueryParams();
+  const queryClient = useQueryClient();
+
   return useMutation({
     mutationKey: ["add_review"],
     mutationFn: (data: FormData) => add_review(data, params.productId || ""),
+    onSuccess: () => {
+      queryClient.invalidateQueries({
+        queryKey: ["get_reviews_by_product_id_infinite"],
+      });
+      queryClient.invalidateQueries({ queryKey: ["get_product_by_id"] });
+    },
   });
 };
 
+// Like / Dislike / Helpful Review
 export const useLikeDislikeHelpfulReview = () => {
   return useMutation({
     mutationKey: ["update_like_dislike_helpful"],
@@ -41,9 +57,13 @@ export const useGetAllReviewsByProductId = ({
     ],
     queryFn: () => get_reviews_by_product_id({ data, queryParams, pageParams }),
     enabled,
+    placeholderData: keepPreviousData,
+    staleTime: 30 * 1000,
+    gcTime: 5 * 60 * 1000,
   });
 };
 
+// Infinite Reviews Query
 export const useGetAllReviewsByProductIdInfinite = ({
   pageParams,
   queryParams,
@@ -63,6 +83,8 @@ export const useGetAllReviewsByProductIdInfinite = ({
     },
     enabled,
     refetchOnWindowFocus,
+    staleTime: 0.5 * 60 * 1000, // 30 seconds
+    gcTime: 5 * 60 * 1000, // 5 minutes
     getNextPageParam: (lastPage, allPages) => {
       const hasMore = lastPage.reviews.length === pageParams.limit;
       return hasMore ? allPages.length + 1 : undefined;
