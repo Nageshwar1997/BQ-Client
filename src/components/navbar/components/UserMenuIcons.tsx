e1770d34332c3366e477f4cd42c4af8ba061c3e9
nageshwarp1997 <nageshwar@ctruh.com>
2025-08-19 15:01:16 +0530

    `Added new icons and features to UserMenuIcons component`

--- src/components/navbar/components/UserMenuIcons.tsx
+++ src/components/navbar/components/UserMenuIcons.tsx
@@ -1,9 +1,12 @@
 import { useEffect, useState } from "react";
 import {
   BuildingIcon,
+  GiftCardIcon,
   HeartIcon,
+  PercentCircleIcon,
   SearchIcon,
   ShoppingBag,
+  TrackIcon,
   UserCircleIcon,
 } from "../../../icons";
 import DarkMode from "../../DarkMode";
@@ -12,6 +15,7 @@ import SearchModal from "../../modal/children/SearchModal";
 import useQueryParams from "../../../hooks/useQueryParams";
 import { useUserStore } from "../../../store/user.store";
 import useOutsideClick from "../../../hooks/useOutsideClick";
+import Button from "../../button/Button";
 
 const UserPopup = ({
   isOpen,
@@ -20,7 +24,8 @@ const UserPopup = ({
   isOpen: boolean;
   onClose: () => void;
 }) => {
-  const { user } = useUserStore();
+  const { navigate } = useQueryParams();
+  const { user, isAuthenticated, logout } = useUserStore();
 
   if (!isOpen) return null;
 
@@ -33,23 +38,71 @@ const UserPopup = ({
         isOpen={isOpen}
         onClose={onClose}
         containerClassName="relative p-0 !rounded-lg"
-        className="!bg-platinum-jet !rounded-lg [&>svg]:hidden"
+        className="!bg-platinum-jet !rounded-lg [&>svg]:hidden !p-4"
       >
-        <div className="flex flex-col items-center gap-2">
-          <div className="w-10 h-10">
-            {user?.profilePic ? (
-              <img
-                src={user.profilePic}
-                alt={user.firstName}
-                className="w-full h-full border-2 border-secondary rounded-full object-cover aspect-square"
+        <div className="flex flex-col gap-2">
+          <div className="flex flex-col items-center gap-2">
+            <div className="w-10 h-10">
+              {user?.profilePic ? (
+                <img
+                  src={user.profilePic}
+                  alt={user.firstName}
+                  className="w-full h-full border-2 border-secondary rounded-full object-cover aspect-square"
+                />
+              ) : (
+                <UserCircleIcon className="stroke-secondary w-full h-full" />
+              )}
+            </div>
+            <p className="text-sm capitalize line-clamp-1 font-semibold">
+              {user ? `${user.firstName} ${user.lastName}` : "Guest"}
+            </p>
+          </div>
+          <hr className="h-px block border-none bg-gradient-line" />
+          <div className="flex flex-col gap-2.5 py-1">
+            <div className="flex items-center gap-2 cursor-pointer">
+              <UserCircleIcon className="w-5 h-5 !stroke-tertiary" />
+              <p className="text-sm/none text-tertiary">My Profile</p>
+            </div>
+            <div className="flex items-center gap-2 cursor-pointer">
+              <TrackIcon className="w-5 h-5 !stroke-tertiary" />
+              <p className="text-sm/none text-tertiary">Track Orders</p>
+            </div>
+            <div className="flex items-center gap-2 cursor-pointer">
+              <GiftCardIcon className="w-5 h-5 fill-tertiary" />
+              <p className="text-sm/none text-tertiary">Rewards</p>
+            </div>
+            <div className="flex items-center gap-2 cursor-pointer">
+              <ShoppingBag className="w-5 h-5 !stroke-tertiary" />
+              <p className="text-sm/none text-tertiary">Cart</p>
+            </div>
+            <div className="flex items-center gap-2 cursor-pointer">
+              <PercentCircleIcon className="w-5 h-5 !stroke-tertiary" />
+              <p className="text-sm/none text-tertiary">Offers</p>
+            </div>
+          </div>
+          <div className="flex justify-between items-center gap-2">
+            <Button
+              content={isAuthenticated ? "Logout" : "Login"}
+              pattern="primary"
+              className="!p-1.5 !rounded !text-xs"
+              onClick={() => {
+                if (!isAuthenticated) {
+                  navigate("/login");
+                } else {
+                  logout();
+                  navigate("/");
+                }
+              }}
+            />
+            {!isAuthenticated && (
+              <Button
+                content="Register"
+                pattern="secondary"
+                className="!p-1.5 !rounded !text-xs"
+                onClick={() => navigate("/register")}
               />
-            ) : (
-              <UserCircleIcon className="stroke-secondary w-full h-full" />
             )}
           </div>
-          <p className="text-sm capitalize line-clamp-1 font-semibold">
-            {user ? `${user.firstName} ${user.lastName}` : "Guest"}
-          </p>
         </div>
       </Modal>
     </div>
@@ -70,13 +123,11 @@ const UserMenuIcons = ({
   });
   const { paths } = useQueryParams();
 
-
-  // Todo: Uncommit this useEffect before push
-  // useEffect(() => {
-  //   if (closeOnNavbarLeave) {
-  //     setOpenUserPopup(false);
-  //   }
-  // }, [closeOnNavbarLeave]);
+  useEffect(() => {
+    if (closeOnNavbarLeave) {
+      setOpenUserPopup(false);
+    }
+  }, [closeOnNavbarLeave]);
 
   return (
     <>
@@ -94,7 +145,7 @@ const UserMenuIcons = ({
           <UserCircleIcon
             onClick={() => setOpenUserPopup((prev) => !prev)}
             className={`w-5 h-5 md:w-6 md:h-6 cursor-pointer ${
-              openUserPopup ? "!stroke-blue-crayola-c" : "stroke-tertiary"
+              openUserPopup ? "!stroke-blue-crayola-c" : "!stroke-tertiary"
             } hover:stroke-secondary`}
           />
           <UserPopup
