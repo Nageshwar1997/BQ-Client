31e8bea15a63e8a29a702a9ecce9c33e28fc3b76
nageshwarp1997 <nageshwar@ctruh.com>
2025-09-06 22:11:26 +0530

    Removed Page Param

--- src/pages/product/searchProducts/SearchProducts.tsx
+++ src/pages/product/searchProducts/SearchProducts.tsx
@@ -54,7 +54,7 @@ const SearchProducts = () => {
         ],
         populateFields: { category: ["name"], reviews: ["rating"] },
       },
-      pageParams: { page: 1, limit: 5 },
+      pageParams: { limit: 5 },
       queryParams: {
         ...(queryParams && queryParams),
         ...(debouncedQuery && { search: debouncedQuery }),
