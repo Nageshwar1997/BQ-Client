cdfee453c1d8eeaacd24a400830761797d45fd0e
nageshwarp1997 <nageshwar@ctruh.com>
2025-09-07 15:01:22 +0530

    Cart Skeleton Added

--- src/pages/cart/Cart.tsx
+++ src/pages/cart/Cart.tsx
@@ -8,6 +8,9 @@ import Button from "../../components/button/Button";
 import { RightArrowIcon } from "../../icons";
 import CartItem from "./children/CartItem";
 import { ICart, TCartProduct } from "../../types";
+import ShowError from "../../components/errors/ShowError";
+import CartSkeleton from "../../components/skeletons/children/CartSkeleton";
+import EmptyData from "../../components/empty-data/EmptyData";
 
 const Cart = () => {
   const { data, isLoading, isError, refetch } = useGetUserCart();
@@ -61,58 +64,70 @@ const Cart = () => {
     );
   };
 
-  if (isLoading) return <p>Loading...</p>;
-  if (isError) return <p>Something went wrong</p>;
-
   return (
-    <div className="min-h-[50dvh] bg-primary-inverted p-4 lg:p-8">
-      <div className="flex flex-col lg:flex-row gap-4">
-        {/* Left - Cart Items */}
-        <div className="flex-1 p-6">
-          <h2 className="text-2xl font-semibold text-secondary mb-6">
-            Your Cart
-          </h2>
-          <div className="h-full space-y-6">
-            {products.map((item) => (
-              <CartItem
-                key={item._id}
-                item={item}
-                onQuantityChange={handleQuantityChange}
-                onRemoveItem={handleRemoveItemFromCart}
-              />
-            ))}
-          </div>
-        </div>
-        {/* Right - Order Summary */}
-        <div className="p-6 h-fit lg:sticky top-16">
-          <h2 className="text-2xl font-semibold text-secondary mb-6">
-            Order Summary
-          </h2>
-          <div className="space-y-4">
-            <div className="flex justify-between text-tertiary">
-              <span>Subtotal</span>
-              <span>₹{subtotal}</span>
-            </div>
-            <div className="flex justify-between text-tertiary">
-              <span>Shipping</span>
-              <span>{shipping === 0 ? "Free" : `₹${shipping}`}</span>
-            </div>
-            <div className="border-t pt-4 flex justify-between text-lg font-semibold text-primary">
-              <span>Total</span>
-              <span>₹{total}</span>
+    <div className="bg-primary-inverted p-4 lg:p-8">
+      {isLoading ? (
+        <CartSkeleton />
+      ) : isError ? (
+        <ShowError
+          headingText="Something went wrong!"
+          descriptionText="Failed to get the product. Please reload the page"
+          className="w-full h-full border border-[red] mx-auto [&>h3]:text-base [&>h3]:base:text-base [&>h3]:sm:text-xl [&>h3]:md:text-2xl [&>h3]:lg:text-3xl [&>h3]:xl:text-4xl [&>h3]:uppercase [&>p]:text-xs [&>p]:base:text-sm [&>p]:sm:text-base [&>p]:md:text-lg"
+        />
+      ) : Object.keys(cart).length > 0 && cart?.products?.length > 0 ? (
+        <div className="flex flex-col lg:flex-row gap-4">
+          {/* Left - Cart Items */}
+          <div className="flex-1 p-6">
+            <h2 className="text-2xl font-semibold text-secondary mb-6">
+              Your Cart
+            </h2>
+            <div className="h-full space-y-6">
+              {products.map((item) => (
+                <CartItem
+                  key={item._id}
+                  item={item}
+                  onQuantityChange={handleQuantityChange}
+                  onRemoveItem={handleRemoveItemFromCart}
+                />
+              ))}
             </div>
           </div>
-          <Button
-            pattern="primary"
-            content="Proceed to Checkout"
-            className="!rounded-lg mt-4 !p-3 gap-2"
-            rightIcon={<RightArrowIcon className="stroke-white" />}
-          />
-          <p className="mt-3 text-sm text-silver-jet text-center">
-            Secure checkout • 100% satisfaction guaranteed
-          </p>
+          {/* Right - Order Summary */}
+          <div className="p-6 h-fit lg:sticky top-16">
+            <h2 className="text-2xl font-semibold text-secondary mb-6">
+              Order Summary
+            </h2>
+            <div className="space-y-4">
+              <div className="flex justify-between text-tertiary">
+                <span>Subtotal</span>
+                <span>₹{subtotal}</span>
+              </div>
+              <div className="flex justify-between text-tertiary">
+                <span>Shipping</span>
+                <span>{shipping === 0 ? "Free" : `₹${shipping}`}</span>
+              </div>
+              <div className="border-t pt-4 flex justify-between text-lg font-semibold text-primary">
+                <span>Total</span>
+                <span>₹{total}</span>
+              </div>
+            </div>
+            <Button
+              pattern="primary"
+              content="Proceed to Checkout"
+              className="!rounded-lg mt-4 !p-3 gap-2"
+              rightIcon={<RightArrowIcon className="stroke-white" />}
+            />
+            <p className="mt-3 text-sm text-silver-jet text-center">
+              Secure checkout • 100% satisfaction guaranteed
+            </p>
+          </div>
         </div>
-      </div>
+      ) : (
+        <EmptyData
+          content={"Your cart is empty!"}
+          className="w-full [&>h3]:text-base [&>h3]:base:text-base [&>h3]:sm:text-xl [&>h3]:md:text-2xl [&>h3]:lg:text-3xl [&>h3]:xl:text-4xl [&>h3]:uppercase gap-5"
+        />
+      )}
     </div>
   );
 };
