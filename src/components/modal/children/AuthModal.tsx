a1e23cd0ac05be1a6b62405c90f0a0a8793e514c
nageshwarp1997 <nageshwar@ctruh.com>
2025-09-14 14:33:48 +0530

    `Added onLoginSuccess callback to AuthModal component`

--- src/components/modal/children/AuthModal.tsx
+++ src/components/modal/children/AuthModal.tsx
@@ -3,20 +3,26 @@ import LoginForm from "../../forms/LoginForm";
 import useQueryParams from "../../../hooks/useQueryParams";
 import { useUserStore } from "../../../store/user.store";
 
-const AuthModal = () => {
+const AuthModal = ({ onLoginSuccess }: { onLoginSuccess?: () => void }) => {
   const { queryParams, removeParam } = useQueryParams();
   const { isAuthenticated } = useUserStore();
 
-  if (isAuthenticated) {
-    removeParam("login");
-    return null;
-  }
+  if (isAuthenticated && !queryParams.login) return null;
 
   return (
     <Modal
       isOpen={queryParams.login === "true"}
       onClose={() => removeParam("login")}
-      children={<LoginForm />}
+      children={
+        <LoginForm
+          onLoginSuccess={() => {
+            removeParam("login");
+            if (onLoginSuccess) {
+              onLoginSuccess();
+            }
+          }}
+        />
+      }
     />
   );
 };
