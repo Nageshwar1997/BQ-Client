0984f6a20d597c63823d33b4dddc6c43c142ab1c
nageshwarp1997 <nageshwar@ctruh.com>
2025-09-27 22:54:04 +0530

    Modal Updated for Title

--- src/components/modal/index.tsx
+++ src/components/modal/index.tsx
@@ -1,6 +1,7 @@
 import { JSX, useEffect } from "react";
 import { CloseIcon } from "../../icons";
 import useVerticalScrollable from "../../hooks/useVerticalScrollable";
+import { BottomGradient, TopGradient } from "../Gradients";
 
 interface ModalProps {
   isOpen: boolean;
@@ -42,20 +43,43 @@ const Modal = ({
       onClick={onClose}
     >
       <div
-        className={`bg-primary-inverted rounded-xl shadow-[var(--primary-8)_0px_4px_16px,_var(--primary-10)_0px_8px_32px] px-6 w-full max-w-md max-h-[90vh] overflow-y-scroll scroll-smooth overflow-hidden relative border border-[red] ${className}`}
+        className={`bg-primary-inverted rounded-xl shadow-[var(--primary-8)_0px_4px_16px,_var(--primary-10)_0px_8px_32px] w-full max-w-md max-h-[90vh] relative ${className}`}
         onClick={(e) => e.stopPropagation()} // prevent closing when clicking inside
       >
-        {/* Sticky Header */}
-        <div className="sticky top-0 bg-primary-inverted flex items-center justify-between py-4 z-50 border-b border-b-primary-50">
-          {heading && <h2 className="text-2xl font-semibold">{heading}</h2>}
-          <CloseIcon
-            onClick={onClose}
-            className="w-4 h-4 sm:w-5 sm:h-5 stroke-tertiary hover:stroke-primary cursor-pointer"
-          />
+        {/* Scrollable area */}
+        {showGradient.top && (
+          <TopGradient className={`h-8 ${heading ? "!top-16" : ""}`} />
+        )}
+        <div
+          ref={containerRef}
+          className={`max-h-[90dvh] overflow-y-auto scroll-smooth px-6 ${
+            heading ? "pb-4" : "py-6"
+          }`}
+        >
+          {/* Sticky Header */}
+          <div
+            className={`z-20 ${
+              heading
+                ? "border-b border-b-primary-50 h-16 flex items-center justify-between sticky top-0 bg-primary-inverted"
+                : ""
+            }`}
+          >
+            {heading && (
+              <h2 className="text-lg sm:text-xl md:text-2xl font-semibold flex-1 text-center bg-clip-text text-transparent bg-silver-duo">
+                {heading}
+              </h2>
+            )}
+            <CloseIcon
+              onClick={onClose}
+              className={`w-4 h-4 sm:w-5 sm:h-5 stroke-tertiary hover:stroke-primary cursor-pointer ${
+                !heading ? "absolute top-2.5 right-2.5" : ""
+              }`}
+            />
+          </div>
+          <div className="py-2">{children}</div>
         </div>
-
         {/* Content */}
-        <div className="py-2">{children}</div>
+        {showGradient.bottom && <BottomGradient className="h-8" />}
       </div>
     </div>
   );
