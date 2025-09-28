0dc835231e49db99873f608ed7cc5f9e1a322868
nageshwarp1997 <nageshwar@ctruh.com>
2025-09-28 14:36:31 +0530

    Input Error css updated

--- src/components/input/SelectRating.tsx
+++ src/components/input/SelectRating.tsx
@@ -92,8 +92,8 @@ const SelectRating = ({ initialValue = 0, onChange, error }: RatingProps) => {
           <p
             className={`w-full text-start flex gap-1 items-center text-[11px] leading-tight text-red-500`}
           >
-            <InfoIcon className="w-3 h-3 md:w-4 md:h-4 fill-red-500" />
-            <span className="leading-none">{error}</span>
+            <InfoIcon className="min-w-3 min-h-3 w-3 h-3 md:min-w-4 md:min-h-4 md:w-4 md:h-4 fill-red-500" />
+            <span className="leading-none line-clamp-2">{error}</span>
           </p>
         )}
       </div>
