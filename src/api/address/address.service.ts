a7b7eb3ccccf89ba307f3b4e1b78270863d4bc0e
nageshwarp1997 <nageshwar@ctruh.com>
2025-09-28 23:10:28 +0530

    `Update address service to use @tanstack/react-query v4 features`

--- src/api/address/address.service.ts
+++ src/api/address/address.service.ts
@@ -1,32 +1,46 @@
-import { useMutation, useQuery } from "@tanstack/react-query";
+import {
+  keepPreviousData,
+  useMutation,
+  useQuery,
+  useQueryClient,
+} from "@tanstack/react-query";
 import { add_address, get_user_addresses, update_address } from "./address.api";
 import { toastErrorMessage, toastSuccessMessage } from "../../utils/toasts";
 
 export const useGetUserAddresses = () => {
   return useQuery({
-    queryKey: ["get_user_details"],
+    queryKey: ["get_user_addresses"],
     queryFn: get_user_addresses,
     retry: false,
     enabled: true,
+    staleTime: Infinity,
+    gcTime: Infinity,
+    placeholderData: keepPreviousData,
   });
 };
 
 export const useAddAddress = () => {
+  const queryClient = useQueryClient();
   return useMutation({
     mutationKey: ["add_address"],
     mutationFn: add_address,
     onError: (error) => toastErrorMessage(error),
-    onSuccess: (data) =>
-      toastSuccessMessage(data.message || "Address added successfully"),
+    onSuccess: (data) => {
+      toastSuccessMessage(data.message || "Address added successfully");
+      queryClient.invalidateQueries({ queryKey: ["get_user_addresses"] });
+    },
   });
 };
 
 export const useUpdateAddress = () => {
+  const queryClient = useQueryClient();
   return useMutation({
     mutationKey: ["update_address"],
     mutationFn: update_address,
     onError: (error) => toastErrorMessage(error),
-    onSuccess: (data) =>
-      toastSuccessMessage(data.message || "Address updated successfully"),
+    onSuccess: (data) => {
+      toastSuccessMessage(data.message || "Address updated successfully");
+      queryClient.invalidateQueries({ queryKey: ["get_user_addresses"] });
+    },
   });
 };
