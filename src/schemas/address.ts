44fd2135d14bcb34326522f1eeb1aed7d4dbd62a
nageshwarp1997 <nageshwar@ctruh.com>
2025-10-07 14:58:38 +0530

    schema updated

--- src/schemas/address.ts
+++ src/schemas/address.ts
@@ -59,7 +59,7 @@ export const addressSchema = z.object({
     showingFieldName: "Email",
     blockSingleSpace: true,
     customRegexes: [{ regex: regexes.validEmail, message: "must be a valid" }],
-  }),
+  }).toLowerCase(),
   altPhoneNumber: zodStringOptional({
     field: "altPhoneNumber",
     showingFieldName: "Alternate phone number",
