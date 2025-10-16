4645907a1c52b1c35a8adf6702ceb895acbd496a
nageshwarp1997 <nageshwar@ctruh.com>
2025-10-16 15:12:33 +0530

    `Remove onSuccess and onError callbacks from useMutation in order.service.ts`

--- src/api/order/order.service.ts
+++ src/api/order/order.service.ts
@@ -2,17 +2,9 @@ import { useMutation } from "@tanstack/react-query";
 import { create_order, verify_payment } from "./order.api";
 
 export const useCreateOrder = () => {
-  return useMutation({
-    mutationFn: create_order,
-    onSuccess: (data) => {},
-    onError: (error: unknown) => {},
-  });
+  return useMutation({ mutationFn: create_order });
 };
 
 export const useVerifyPayment = () => {
-  return useMutation({
-    mutationFn: verify_payment,
-    onSuccess: (data) => {},
-    onError: (error: unknown) => {},
-  });
+  return useMutation({ mutationFn: verify_payment });
 };
