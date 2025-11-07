4a82bfb5ecaaf22ce7cbe1f2d1964955558612a0
nageshwarp1997 <nageshwar@ctruh.com>
2025-11-07 10:15:54 +0530

    `Added RefreshIcon and updated OpeningModal with form submission functionality`

--- src/pages/company/children/JobApplicationForm.tsx
+++
@@ -1,8 +0,0 @@
-import { TRoleOpening } from "../../../types";
-
-const JobApplicationForm = ({ opening }: { opening: TRoleOpening }) => {
-  console.log("OPENING", opening);
-  return <div>JobApplicationForm</div>;
-};
-
-export default JobApplicationForm;
