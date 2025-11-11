1b99eca16afccd24f4a0f0233064835f0597b38c
nageshwarp1997 <nageshwar@ctruh.com>
2025-11-11 14:03:07 +0530

    Removed mt-2 css

--- src/components/input/FileInput.tsx
+++ src/components/input/FileInput.tsx
@@ -97,7 +97,7 @@ const FileInput = ({
           {errors?.map((error, index) => (
             <p
               key={index}
-              className={`w-full text-start flex gap-1 items-center text-[11px] leading-tight mt-2 text-red-500`}
+              className={`w-full text-start flex gap-1 items-center text-[11px] leading-tight text-red-500`}
             >
               <InfoIcon className="min-w-3 min-h-3 w-3 h-3 md:min-w-4 md:min-h-4 md:w-4 md:h-4 fill-red-500" />
               <span className="leading-none line-clamp-2">{error}</span>
