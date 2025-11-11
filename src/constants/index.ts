fe608a54a2b3ae91aef08df004a41b311684453a
nageshwarp1997 <nageshwar@ctruh.com>
2025-11-11 20:42:49 +0530

    `Updated regex for valid PAN number to allow lowercase letters`

--- src/constants/index.ts
+++ src/constants/index.ts
@@ -95,7 +95,7 @@ export const regexes: Record<TRegexes, RegExp> = {
   validGST: /^[0-9]{2}[A-Z]{5}[0-9]{4}[A-Z]{1}[1-9A-Z]{1}Z[0-9A-Z]{1}$/i, // Check valid GST number
   validUrl:
     /^(https?:\/\/)?([\w-]+\.)+[\w-]{2,}(\/[\w\-._~:/?#[\]@!$&'()*+,;=%]*)?$/i,
-  validPan: /^[A-Z]{5}[0-9]{4}[A-Z]$/,
+  validPan: /^[A-Za-z]{5}[0-9]{4}[A-Za-z]$/,
 };
 
 export const reviewInitialValues = {
