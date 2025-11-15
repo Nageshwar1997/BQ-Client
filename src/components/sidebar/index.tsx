2703fee20c352c0f0bbe33ee77ff2bbeca3d2d84
nageshwarp1997 <nageshwar@ctruh.com>
2025-11-15 14:52:50 +0530

    Sidebar Done

--- src/components/sidebar/index.tsx
+++ src/components/sidebar/index.tsx
@@ -1,17 +1,20 @@
-import { DropdownIcon, SettingIcon, UserCircleIcon } from "../../icons";
+import { DropdownIcon, UserCircleIcon } from "../../icons";
 import { SIDEBAR_DATA } from "../../constants";
 import usePathParams from "../../hooks/usePathParams";
 import { useUserStore } from "../../store/user.store";
+import useVerticalScrollable from "../../hooks/useVerticalScrollable";
+import { BottomGradient, TopGradient } from "../Gradients";
 
 const Sidebar = () => {
   const { navigate } = usePathParams();
-  const { user } = useUserStore();
+  const { user, logout } = useUserStore();
+  const { showGradient, containerRef } = useVerticalScrollable();
 
   return (
-    <div className="max-w-20 lg:max-w-[250px] w-full max-h-[calc(100dvh-64px)] lg:max-h-[calc(100dvh-100px)] grow bg-secondary-inverted text-tertiary sticky top-16 lg:top-[100px]">
+    <div className="hidden base:block max-w-20 lg:max-w-[250px] w-full max-h-[calc(100dvh-64px)] lg:max-h-[calc(100dvh-100px)] grow bg-secondary-inverted text-tertiary sticky top-16 lg:top-[100px] border-r border-r-primary-30">
       <div className="w-full h-full flex flex-col gap-3">
         {/* Profile Section */}
-        <div className="w-full flex flex-col lg:flex-row justify-center items-center gap-1 px-2 py-3">
+        <div className="w-full flex flex-col lg:flex-row justify-center items-center gap-1 px-2 py-3 border-b border-b-primary-30">
           <div className="w-8 h-8 lg:h-14 lg:w-14 overflow-hidden p-px bg-accent-duo rounded-full shadow-primary-btn">
             {user?.profilePic ? (
               <img
@@ -33,43 +36,49 @@ const Sidebar = () => {
             </p>
           </div>
         </div>
-        <div className="w-full grow">
-          <div className="w-full relative flex flex-col justify-between items-center h-full">
-            <div className="w-full h-full overflow-y-scroll p-2 flex flex-col gap-3 pb-10 items-center">
-              {SIDEBAR_DATA.map((item, index) => {
-                const Icon = item.icon;
-                return (
-                  <div
-                    key={index}
-                    onClick={() => navigate(`${item.path}`)}
-                    className="w-fit lg:w-full flex items-center justify-between gap-2 group cursor-pointer p-2 border border-primary-10 rounded-lg hover:bg-primary-inverted-10 shadow-lg hover:shadow-primary-inverted-50 light:hover:shadow-primary-50 hover:scale-[1.02] duration-300"
-                  >
-                    <div className="flex items-center gap-2">
-                      <Icon
-                        className={`w-5 h-5 transition-colors duration-300 ${item.className}`}
-                      />
-                      <p className="text-tertiary group-hover:text-primary text-base hidden lg:block">
-                        {item.label}
-                      </p>
-                    </div>
-                    <DropdownIcon className="-rotate-90 stroke-tertiary group-hover:stroke-primary hidden lg:block" />
+        <div className="w-full h-[calc(100%-100px)] relative">
+          {showGradient.top && (
+            <TopGradient className="!w-full h-8 -top-px from-secondary-inverted" />
+          )}
+          <div
+            className="w-full h-full overflow-y-scroll p-2 flex flex-col gap-3 items-center"
+            ref={containerRef}
+          >
+            {SIDEBAR_DATA.map((item, index) => {
+              const Icon = item.icon;
+              return (
+                <div
+                  key={index}
+                  onClick={() =>
+                    item.path
+                      ? navigate(item.path)
+                      : item.label === "Logout"
+                      ? logout()
+                      : null
+                  }
+                  className="w-fit lg:w-full flex items-center justify-between gap-2 group cursor-pointer p-2 border border-primary-10 rounded-lg hover:bg-primary-inverted-10 shadow-lg hover:shadow-primary-inverted-50 light:hover:shadow-primary-50 hover:scale-[1.02] duration-300"
+                >
+                  <div className="flex items-center gap-2">
+                    <Icon
+                      className={`w-5 h-5 transition-colors duration-300 ${item.className}`}
+                    />
+                    <p className="text-tertiary group-hover:text-primary text-base hidden lg:block">
+                      {item.label}
+                    </p>
                   </div>
-                );
-              })}
-            </div>
-            <div className="px-2 w-fit lg:w-full">
-              <div className="w-full flex items-center justify-between gap-2 group cursor-pointer p-2 border border-primary-10 rounded-lg hover:bg-primary-inverted-10 shadow-lg hover:shadow-primary-inverted-50 light:hover:shadow-primary-50 hover:scale-[1.02] duration-300 mb-4">
-                <div className="flex items-center gap-2">
-                  <SettingIcon className="w-5 h-5 stroke-tertiary group-hover:stroke-primary transition-colors duration-300" />
-                  <p className="text-tertiary group-hover:text-primary text-base hidden lg:block">
-                    Settings
-                  </p>
+                  <DropdownIcon
+                    className={`-rotate-90 stroke-tertiary group-hover:stroke-primary hidden ${
+                      item.label !== "Logout" ? "lg:block" : ""
+                    }`}
+                  />
                 </div>
-                <DropdownIcon className="-rotate-90 stroke-tertiary group-hover:stroke-primary hidden lg:block" />
-              </div>
-            </div>
+              );
+            })}
           </div>
         </div>
+        {showGradient.bottom && (
+          <BottomGradient className="!w-full bottom-0 h-8 from-secondary-inverted" />
+        )}
       </div>
     </div>
   );
