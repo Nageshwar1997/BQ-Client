18179e7b041a45000e38f39c6e0ffe3e2441396e
nageshwarp1997 <nageshwar@ctruh.com>
2025-12-31 12:56:33 +0530

    Upload Profile Component logic changed

--- src/pages/auth/components/UploadProfile.tsx
+++ src/pages/auth/components/UploadProfile.tsx
@@ -5,13 +5,12 @@ import { toastErrorMessage } from "../../../utils/toasts";
 import usePathParams from "../../../hooks/usePathParams";
 
 const UploadProfile = ({
-  name = "image",
-  previewImage = "",
-  onChange,
+  previewUrl = "",
   className,
-  errorText,
+  fileInputProps = {},
+  error,
 }: ProfilePicInputProps) => {
-  const [previewUrl, setPreviewUrl] = useState<string>(previewImage);
+  const [src, setSrc] = useState<string>(previewUrl);
 
   const handleImageChange = useCallback(
     (e: ChangeEvent<HTMLInputElement>) => {
@@ -25,16 +24,16 @@ const UploadProfile = ({
         }
 
         const blobUrl = URL.createObjectURL(file);
-        setPreviewUrl(blobUrl);
+        setSrc(blobUrl);
 
-        onChange(file);
+        fileInputProps?.onChange?.(file);
 
         return () => URL.revokeObjectURL(blobUrl);
       }
-      setPreviewUrl("");
-      onChange(null);
+      setSrc("");
+      fileInputProps.onChange?.(null);
     },
-    [onChange]
+    [fileInputProps]
   );
   const { pathname } = usePathParams();
   return (
@@ -42,10 +41,15 @@ const UploadProfile = ({
       <div
         className={`w-20 h-20 max-w-20 max-h-20 mx-auto rounded-full shadow-primary-btn-hover overflow-hidden group ${className}`}
       >
-        <label htmlFor={name} className="relative cursor-pointer">
+        <label
+          htmlFor={
+            fileInputProps?.id || fileInputProps?.name || "upload-profile"
+          }
+          className="relative cursor-pointer"
+        >
           <div className="absolute inset-0 bg-gradient-to-br from-primary-inverted via-transparent to-gray-400 rounded-full blur-sm animate-pulse cursor-pointer" />
           <img
-            src={previewUrl || "/images/logo/BQ_white_logo.webp"}
+            src={src || "/images/logo/BQ_white_logo.webp"}
             alt="Profile Picture"
             className="object-cover bg-accent-duo rounded-full w-full h-full shadow-inner"
             loading="lazy"
@@ -54,10 +58,11 @@ const UploadProfile = ({
             <div className="absolute bottom-0 left-0 right-0 bg-smoke-eerie flex items-center justify-center">
               <CameraIcon className="fill-primary opacity-70 group-hover:opacity-100" />
               <input
-                id={name}
-                name={name}
+                {...fileInputProps}
+                id={fileInputProps?.id || fileInputProps?.name || "profile-pic"}
                 type="file"
                 className="hidden"
+                multiple={false}
                 accept="image/webp, image/png, image/jpeg, image/jpg"
                 onChange={handleImageChange}
               />
@@ -65,10 +70,10 @@ const UploadProfile = ({
           )}
         </label>
       </div>
-      {errorText && (
+      {error && (
         <div className="w-full flex gap-1 justify-center items-center text-[11px] leading-tight mt-2 text-red-500">
           <InfoIcon className="min-w-3 min-h-3 w-3 h-3 md:min-w-4 md:min-h-4 md:w-4 md:h-4 fill-red-500" />
-          <span className="leading-none">{errorText}</span>
+          <span className="leading-none">{error}</span>
         </div>
       )}
     </div>
