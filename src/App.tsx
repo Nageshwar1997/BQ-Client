fd873b8688dd8c514d4a999f242d006844112897
nageshwarp1997 <nageshwar@ctruh.com>
2026-01-12 19:42:41 +0530

    App.tsx Done

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,31 @@
-import { useEffect } from "react";
-import { RouterProvider } from "react-router-dom";
-import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
-import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
-import { Toaster } from "react-hot-toast";
-import router from "./routes";
-import useThemeStore from "./store/theme.store";
-import { VITE_IS_DEV } from "./envs";
+import { useEffect } from 'react';
+import { RouterProvider } from 'react-router-dom';
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
+import { Toaster } from 'react-hot-toast';
+import { router } from './routes';
+import { useThemeStore } from './store';
+import { VITE_IS_DEV } from './envs';
 
 function App() {
   const { theme } = useThemeStore();
   const queryClient = new QueryClient();
 
   useEffect(() => {
-    document.body.setAttribute("theme", theme);
+    document.documentElement.setAttribute('theme', theme);
   }, [theme]);
 
   return (
     <QueryClientProvider client={queryClient}>
       <Toaster position="top-center" />
-      <div className="max-w-dvw max-h-dvh min-w-dvw min-h-dvh w-full h-full overflow-y-scroll bg-primary-inverted text-primary">
-        <div className="max-w-480 w-full h-full mx-auto">
+      <div className="bg-primary-inverted text-primary h-full max-h-dvh min-h-dvh w-full max-w-dvw min-w-dvw overflow-y-scroll">
+        <div className="mx-auto h-full w-full max-w-480">
           <RouterProvider router={router} />
         </div>
       </div>
       {/* React Query Devtools */}
-      {VITE_IS_DEV === "true" && (
-        <ReactQueryDevtools
-          initialIsOpen={false}
-          position="bottom"
-          buttonPosition="bottom-left"
-        />
+      {VITE_IS_DEV === 'true' && (
+        <ReactQueryDevtools initialIsOpen={false} position="bottom" buttonPosition="bottom-left" />
       )}
     </QueryClientProvider>
   );
