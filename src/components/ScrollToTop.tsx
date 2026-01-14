2ea53a0ed39b5d3a29164ddc8714a18eaad1f53b
nageshwarp1997 <nageshwar@ctruh.com>
2026-01-14 13:51:42 +0530

    Add ScrollToTop component and update exports

---
+++ src/components/ScrollToTop.tsx
@@ -0,0 +1,15 @@
+import { useEffect } from 'react';
+import { useQueryParams } from '../hooks';
+
+export const ScrollToTop = () => {
+  const { pathname } = useQueryParams();
+
+  useEffect(() => {
+    const el = document.getElementById('main');
+
+    if (!el) return;
+    el.scrollIntoView({ behavior: 'auto', block: 'start' });
+  }, [pathname]);
+
+  return null;
+};
---
+++ src/components/index.ts
@@ -0,0 +1,2 @@
+export * from './ui/Theme';
+export * from './ScrollToTop';
