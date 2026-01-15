921b0b54810d95ab90386c2853386d0c4069cbce
nageshwarp1997 <nageshwar@ctruh.com>
2026-01-15 15:34:29 +0530

    animate added

--- src/components/Test.tsx
+++ src/components/Test.tsx
@@ -14,7 +14,7 @@ const Test = () => {
       <div className="pl__dot"></div>
       <div className="pl__dot"></div>
       <div className="pl__dot"></div>
-      <div className="bg-accent-duo max-w-30 -rotate-45 gap-0.5 bg-clip-text text-center text-sm font-bold text-transparent">
+      <div className="bg-accent-duo max-w-30 -rotate-45 animate-pulse gap-0.5 bg-clip-text text-center text-sm font-bold text-transparent">
         Loading Please&nbsp;wait...
       </div>
     </div>
