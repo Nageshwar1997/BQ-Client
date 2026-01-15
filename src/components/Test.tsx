fa8bbb8c14dc205b2d00f28f460932197ad08b21
nageshwarp1997 <nageshwar@ctruh.com>
2026-01-15 15:32:00 +0530

    Loading Text Done

---
+++ src/components/Test.tsx
@@ -0,0 +1,24 @@
+import './test.css';
+const Test = () => {
+  return (
+    <div className="pl">
+      <div className="pl__dot"></div>
+      <div className="pl__dot"></div>
+      <div className="pl__dot"></div>
+      <div className="pl__dot"></div>
+      <div className="pl__dot"></div>
+      <div className="pl__dot"></div>
+      <div className="pl__dot"></div>
+      <div className="pl__dot"></div>
+      <div className="pl__dot"></div>
+      <div className="pl__dot"></div>
+      <div className="pl__dot"></div>
+      <div className="pl__dot"></div>
+      <div className="bg-accent-duo max-w-30 -rotate-45 gap-0.5 bg-clip-text text-center text-sm font-bold text-transparent">
+        Loading Please&nbsp;wait...
+      </div>
+    </div>
+  );
+};
+
+export default Test;
