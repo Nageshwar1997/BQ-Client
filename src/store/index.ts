37f740f22f45973faf179d7db17f31a5310d787a
nageshwarp1997 <nageshwar@ctruh.com>
2026-01-19 23:06:21 +0530

    Stores Done

--- src/store/index.ts
+++ src/store/index.ts
@@ -1,2 +1,11 @@
-export * from './theme.store';
-export * from './action.store';
+import { useActionStore } from './action';
+import { useThemeStore } from './theme';
+import { useUserStore } from './user';
+
+class Store {
+  public theme = useThemeStore;
+  public action = useActionStore;
+  public user = useUserStore;
+}
+
+export const store = new Store();
