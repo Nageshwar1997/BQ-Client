68f043065b19bf3f43be63e7c6dc97fbffb574aa
nageshwarp1997 <nageshwar@ctruh.com>
2026-01-28 00:19:08 +0530

    Query CLient Updated

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,22 @@
 import { useEffect } from 'react';
 import { RouterProvider } from 'react-router-dom';
-import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+import { MutationCache, QueryCache, QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
 import { Toaster } from 'react-hot-toast';
 import { router } from './routes';
-import { useThemeStore } from './store';
+import { store } from './store';
 import { VITE_IS_DEV } from './envs';
 
 function App() {
-  const { theme } = useThemeStore();
-  const queryClient = new QueryClient();
+  const { theme } = store.theme();
+  const queryClient = new QueryClient({
+    defaultOptions: {
+      queries: { refetchOnWindowFocus: false, retry: (failureCount) => failureCount < 3 },
+      mutations: { retry: (failureCount) => failureCount < 3 },
+    },
+    queryCache: new QueryCache({ onSuccess: () => console.log('Query success') }),
+    mutationCache: new MutationCache({ onSuccess: () => console.log('Mutation success') }),
+  });
 
   useEffect(() => {
     document.documentElement.setAttribute('theme', theme);
