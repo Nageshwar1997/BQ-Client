d978e9785d9a83685b0b076deed56d632f331432
nageshwarp1997 <nageshwar@ctruh.com>
2026-01-30 02:11:20 +0530

    AddressFormModal Done

---
+++ src/components/layout/modals/AddressFormModal.tsx
@@ -0,0 +1,31 @@
+import { useEffect } from 'react';
+import type { IAddress } from '../../../types';
+import { customHooks } from '../../../hooks';
+import { ModalWrapper } from './ModalWrapper';
+import AddressForm from '../forms/AddressForm';
+
+export const AddressFormModal = ({
+  onClose,
+  addresses,
+}: {
+  onClose: () => void;
+  addresses?: IAddress[];
+}) => {
+  const { queryParams, removeParam } = customHooks.QueryParams();
+
+  useEffect(() => {
+    removeParam('add');
+    removeParam('edit');
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, []);
+  return (
+    <ModalWrapper
+      onClose={onClose}
+      isOpen={!!queryParams.add || !!queryParams.edit}
+      header={{ title: 'Add Address', showCloseIcon: true }}
+      className="max-w-3xl!"
+    >
+      <AddressForm addresses={addresses} className="mt-2" />
+    </ModalWrapper>
+  );
+};
