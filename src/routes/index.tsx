26168e0ebf5334da0a8937a2e520a0c918c982e4
nageshwarp1997 <nageshwar@ctruh.com>
2026-02-22 11:02:29 +0530

    Temp Routers added

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,184 +1,132 @@
-import { createBrowserRouter, Outlet } from "react-router-dom";
-import { lazy, Suspense } from "react";
+import { createBrowserRouter, Outlet } from 'react-router-dom';
+import { Suspense } from 'react';
+import { Auth, Login, Register } from '../pages/Auth';
+import { AuthRedirect } from './AuthRedirect';
+import { LoadingScreen } from '../components';
+import { PrivateRoute } from './PrivateRoute';
 
-// Lazy loaded main layout
-const Main = lazy(() => import("../pages/main/Main"));
-
-// Pages
-import Home from "../pages/home/Home";
-import { NotFound, SomethingWentWrong } from "../pages/error";
-import LoadingScreen from "../components/loaders/LoadingScreen";
-import CategoryProducts from "../pages/product/categoryProducts";
-import Offers from "../pages/offers/Offers";
-import Blogs from "../pages/blogs/Blogs";
-import SearchProducts from "../pages/product/searchProducts/SearchProducts";
-import ProductDetails from "../pages/product/productDetails/ProductDetails";
-import PrivateRoute from "./PrivateRoute";
-import Cart from "../pages/cart/Cart";
-import Login from "../pages/auth/Login";
-import AuthRedirect from "./AuthRedirect";
-import Register from "../pages/auth/Register";
-import Address from "../pages/address";
-import Payment from "../pages/account/payment/Payment";
-import Orders from "../pages/account/order/Orders";
-import OrderDetails from "../pages/account/order/OrderDetails";
-import Account from "../pages/account";
-import AboutUs from "../pages/company/AboutUs";
-import PartnerWithUs from "../pages/company/PartnerWithUs";
-import Careers from "../pages/company/Careers";
-import Sustainability from "../pages/company/Sustainability";
-import Ethics from "../pages/company/Ethics";
-import PressMedia from "../pages/company/PressMedia";
-import Teams from "../pages/company/Teams";
-import Wishlist from "../pages/quick-links/Wishlist";
-import ReferFriend from "../pages/quick-links/ReferFriend";
-import StoreLocator from "../pages/quick-links/StoreLocator";
-import BecomeSeller from "../pages/quick-links/BecomeSeller";
-import ContactUs from "../pages/services/ContactUs";
-import HelpCenterFAQ from "../pages/services/HelpCenterFAQ";
-import ShippingInfo from "../pages/services/ShippingInfo";
-import OrderReturnRefund from "../pages/account/order/OrderReturnRefund";
-import Accessibility from "../pages/legal-policies/Accessibility";
-import Disclaimer from "../pages/legal-policies/Disclaimer";
-import TermsAndConditions from "../pages/legal-policies/TermsAndConditions";
-import CookiePolicy from "../pages/legal-policies/CookiePolicy";
-import PrivacyPolicy from "../pages/legal-policies/PrivacyPolicy";
-import TrackMyOrders from "../pages/account/order/TrackMyOrders";
-import MissionVisionValues from "../pages/company/MissionVisionValues";
-import Awards from "../pages/company/Awards";
-import ValuesAndCulture from "../pages/company/ValuesAndCulture";
-import RetailAndECommerce from "../pages/company/RetailAndECommerce";
-import BlogDetails from "../pages/blogs/BlogDetails";
-import Profile from "../pages/account/profile";
-import MyReviewsAndRating from "../pages/account/reviews-and-ratings";
-import TrackOrder from "../pages/account/order/TrackOrder";
-import OAuthSuccess from "../pages/auth/OAuthSuccess";
-import ResetPassword from "../pages/account/profile/ResetPassword";
-import ForgotPassword from "../pages/account/profile/ForgotPassword";
-
-const router = createBrowserRouter([
+export const router = createBrowserRouter([
   {
-    path: "/",
+    path: '/',
     element: (
       <Suspense fallback={<LoadingScreen />}>
-        <Main />
+        <div>Main</div>
       </Suspense>
     ),
-    errorElement: <SomethingWentWrong />,
+    errorElement: <div>Error Page</div>,
     children: [
-      { index: true, element: <Home /> },
-      { path: "offers", element: <Offers /> },
+      { index: true, element: <div>Home</div> },
+      { path: 'offers', element: <div>Offers</div> },
       {
-        path: "blogs",
+        path: 'blogs',
         element: <Outlet />,
         children: [
-          { index: true, element: <Blogs /> },
-          { path: ":blogId", element: <BlogDetails /> },
+          { index: true, element: <div>Blogs</div> },
+          { path: ':blogId', element: <div>Blog Details</div> },
         ],
       },
-      { path: "search", element: <SearchProducts /> },
-      { path: "product/:productId", element: <ProductDetails /> },
-      { path: "products/:levelOneCategory", element: <CategoryProducts /> },
+      { path: 'search', element: <div>Search Products</div> },
+      { path: 'product/:productId', element: <div>Product Details</div> },
+      { path: 'products/:levelOneCategory', element: <div>Category Products</div> },
       {
-        path: "products/:levelOneCategory/:levelTwoCategory",
-        element: <CategoryProducts />,
+        path: 'products/:levelOneCategory/:levelTwoCategory',
+        element: <div>Category Products</div>,
       },
       {
-        path: "products/:levelOneCategory/:levelTwoCategory/:levelThreeCategory",
-        element: <CategoryProducts />,
+        path: 'products/:levelOneCategory/:levelTwoCategory/:levelThreeCategory',
+        element: <div>Category Products</div>,
       },
-      { path: "refer", element: <PrivateRoute children={<ReferFriend />} /> }, // Todo: Refer Part is Pending
-      { path: "cart", element: <PrivateRoute children={<Cart />} /> },
-      { path: "address", element: <PrivateRoute children={<Address />} /> },
-      { path: "wishlist", element: <PrivateRoute children={<Wishlist />} /> },
+      { path: 'refer', element: <PrivateRoute children={<div>Refer Friend</div>} /> }, // Todo: Refer Part is Pending
+      { path: 'cart', element: <PrivateRoute children={<div>Cart</div>} /> },
+      { path: 'address', element: <PrivateRoute children={<div>Address</div>} /> },
+      { path: 'wishlist', element: <PrivateRoute children={<div>Wishlist</div>} /> },
       {
-        path: "become-seller",
-        element: <PrivateRoute children={<BecomeSeller />} />,
+        path: 'become-seller',
+        element: <PrivateRoute children={<div>Become Seller</div>} />,
       },
       {
-        path: "orders",
+        path: 'orders',
         element: <PrivateRoute children={<Outlet />} />,
         children: [
-          { index: true, element: <Orders /> },
-          { path: ":orderId", element: <OrderDetails /> }, // Todo: Cancel Order and Return Order Pending
-          { path: "return-refund", element: <OrderReturnRefund /> }, // Todo: Pending
+          { index: true, element: <div>Orders</div> },
+          { path: ':orderId', element: <div>Order Details</div> }, // Todo: Cancel Order and Return Order Pending
+          { path: 'return-refund', element: <div>Order Return Refund</div> }, // Todo: Pending
           {
-            path: "track",
+            path: 'track',
             element: <Outlet />,
             children: [
-              { index: true, element: <TrackMyOrders /> },
-              { path: ":orderId", element: <TrackOrder /> },
+              { index: true, element: <div>TrackMyOrders</div> },
+              { path: ':orderId', element: <div>TrackOrder</div> },
             ],
           },
-          { path: "payment", element: <Payment /> },
+          { path: 'payment', element: <div>Payment</div> },
         ],
       },
       {
-        path: "account",
-        element: <PrivateRoute children={<Account />} />,
+        path: 'account',
+        element: <PrivateRoute children={<div>Account</div>} />,
         children: [
           {
             index: true,
-            element: <Profile />,
+            element: <div>Profile</div>,
           }, // Todo: Pending
-          { path: "contact", element: <ContactUs /> },
-          { path: "cart", element: <Cart /> },
-          { path: "reviews-and-ratings", element: <MyReviewsAndRating /> },
+          { path: 'contact', element: <div>ContactUs</div> },
+          { path: 'cart', element: <div>Cart</div> },
+          { path: 'reviews-and-ratings', element: <div>MyReviewsAndRating</div> },
           {
-            path: "track",
+            path: 'track',
             element: <Outlet />,
             children: [
-              { index: true, element: <TrackMyOrders /> },
-              { path: ":orderId", element: <TrackOrder /> },
+              { index: true, element: <div>TrackMyOrders</div> },
+              { path: ':orderId', element: <div>TrackOrder</div> },
             ],
           },
           {
-            path: "orders",
+            path: 'orders',
             element: <PrivateRoute children={<Outlet />} />,
             children: [
-              { index: true, element: <Orders /> },
-              { path: ":orderId", element: <OrderDetails /> }, // Todo: Cancel Order and Return Order Pending
-              { path: "return-refund", element: <OrderReturnRefund /> }, // Todo: Pending
+              { index: true, element: <div>Orders</div> },
+              { path: ':orderId', element: <div>Order Details</div> }, // Todo: Cancel Order and Return Order Pending
+              { path: 'return-refund', element: <div>Order Return Refund</div> }, // Todo: Pending
             ],
           },
         ],
       },
       // Company Pages
-      { path: "about-us", element: <AboutUs /> },
-      { path: "partner-with-us", element: <PartnerWithUs /> },
-      { path: "careers", element: <Careers /> },
-      { path: "mission-vision", element: <MissionVisionValues /> },
-      { path: "sustainability", element: <Sustainability /> },
-      { path: "ethics", element: <Ethics /> },
-      { path: "teams", element: <Teams /> },
-      { path: "press-media", element: <PressMedia /> },
-      { path: "awards", element: <Awards /> },
-      { path: "values-and-culture", element: <ValuesAndCulture /> },
-      { path: "retail-and-e-commerce", element: <RetailAndECommerce /> },
+      { path: 'about-us', element: <div>AboutUs</div> },
+      { path: 'partner-with-us', element: <div>PartnerWithUs</div> },
+      { path: 'careers', element: <div>Careers</div> },
+      { path: 'mission-vision', element: <div>MissionVisionValues</div> },
+      { path: 'sustainability', element: <div>Sustainability</div> },
+      { path: 'ethics', element: <div>Ethics</div> },
+      { path: 'teams', element: <div>Teams</div> },
+      { path: 'press-media', element: <div>PressMedia</div> },
+      { path: 'awards', element: <div>Awards</div> },
+      { path: 'values-and-culture', element: <div>ValuesAndCulture</div> },
+      { path: 'retail-and-e-commerce', element: <div>RetailAndECommerce</div> },
 
       // Quick Link Pages
-      { path: "store-locator", element: <StoreLocator /> },
+      { path: 'store-locator', element: <div>StoreLocator</div> },
       // Services Pages
-      { path: "contact", element: <ContactUs /> },
-      { path: "help-center-faq", element: <HelpCenterFAQ /> },
-      { path: "shipping-info", element: <ShippingInfo /> },
+      { path: 'contact', element: <div>ContactUs</div> },
+      { path: 'help-center-faq', element: <div>HelpCenterFAQ</div> },
+      { path: 'shipping-info', element: <div>ShippingInfo</div> },
       // Legal Policies Pages
-      { path: "privacy-policy", element: <PrivacyPolicy /> },
-      { path: "cookie-policy", element: <CookiePolicy /> },
-      { path: "terms-conditions", element: <TermsAndConditions /> },
-      { path: "disclaimer", element: <Disclaimer /> },
-      { path: "accessibility", element: <Accessibility /> },
+      { path: 'privacy-policy', element: <div>PrivacyPolicy</div> },
+      { path: 'cookie-policy', element: <div>CookiePolicy</div> },
+      { path: 'terms-conditions', element: <div>TermsAndConditions</div> },
+      { path: 'disclaimer', element: <div>Disclaimer</div> },
+      { path: 'accessibility', element: <div>Accessibility</div> },
     ],
   },
-  { path: "register", element: <AuthRedirect children={<Register />} /> },
-  { path: "login", element: <AuthRedirect children={<Login />} /> },
   {
-    path: "forgot-password",
-    element: <AuthRedirect children={<ForgotPassword />} />,
+    path: 'auth',
+    element: <Auth />,
+    children: [
+      { index: true, element: <AuthRedirect children={<Login />} /> },
+      { path: 'register', element: <AuthRedirect children={<Register />} /> },
+      { path: 'oauth', element: <AuthRedirect children={<div>OAuth Success/Error</div>} /> },
+      { path: 'password', element: <AuthRedirect children={<div>Password</div>} /> },
+    ],
   },
-  { path: "oauth-success", element: <OAuthSuccess /> },
-  { path: "reset-password", element: <ResetPassword /> },
-  { path: "*", element: <NotFound /> },
 ]);
-
-export default router;
