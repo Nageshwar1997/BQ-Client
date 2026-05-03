d0ea412731fe6d164ad34e432f86626eece43dfe
Nageshwar <nageshpawarpatil@gmail.com>
2026-05-03 18:41:29 +0530

    fix: add alt attribute and loading attribute to auth image for accessibility

--- src/pages/auth/index.tsx
+++ src/pages/auth/index.tsx
@@ -5,10 +5,12 @@ import { Outlet } from 'react-router-dom';
 const Auth = () => {
   return (
     <div className="relative flex h-dvh min-h-dvh w-full gap-4 p-4 outline-hidden">
-      <div className="hidden w-full rounded-2xl bg-linear-90 from-[#00F] to-transparent p-10 lg:block">
+      <div className="from-blue-crayola-c via-dodger-blue-c hidden w-full rounded-2xl bg-linear-90 to-transparent p-10 lg:block">
         <img
           src="/images/auth/auth-left-side.webp"
           className="aspect-square h-full w-full object-contain"
+          alt="Auth-Image"
+          loading="eager"
         />
       </div>
       <Theme className="bg-secondary-invert stroke-secondary border-primary/30 absolute top-5 right-5 z-10 h-fit rounded-full border p-2 md:p-3" />
