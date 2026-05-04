7bb9e038449509fcedb2f516edefda0defe86af4
Nageshwar <nageshpawarpatil@gmail.com>
2026-05-05 00:02:40 +0530

    Home Video Carousel Added

--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -1,9 +1,11 @@
 import Theme from '@/components/ui/Theme';
 import { Link } from 'react-router-dom';
+import HomeVideoCarousel from './HomeVideoCarousel';
 
 const Home = () => {
   return (
-    <div className="grid gap-6">
+    <div className="h-full w-full space-y-5 lg:-mt-16 lg:space-y-10">
+      <HomeVideoCarousel />
       <Theme />
       <Link to="/auth/change-password">Change Password</Link>
       <Link to="/auth/set-password">Set Password</Link>
