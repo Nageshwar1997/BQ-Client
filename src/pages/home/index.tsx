6e88061eb0d342b7ada16b8b92144e6b69c68123
Nageshwar <nageshpawarpatil@gmail.com>
2026-05-05 00:24:10 +0530

    HomeHero Done

--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -1,11 +1,13 @@
 import Theme from '@/components/ui/Theme';
 import { Link } from 'react-router-dom';
+import HomeHero from './HomeHero';
 import HomeVideoCarousel from './HomeVideoCarousel';
 
 const Home = () => {
   return (
-    <div className="h-full w-full space-y-5 lg:-mt-16 lg:space-y-10">
+    <div className="h-full w-full lg:-mt-16">
       <HomeVideoCarousel />
+      <HomeHero />
       <Theme />
       <Link to="/auth/change-password">Change Password</Link>
       <Link to="/auth/set-password">Set Password</Link>
