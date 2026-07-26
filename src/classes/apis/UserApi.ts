bb7ba6dcdff22c8c5e0c2d1806bff3fa901f14c8
Nageshwar <nageshpawarpatil@gmail.com>
2026-07-26 15:27:42 +0530

    Register apis added

--- src/classes/apis/UserApi.ts
+++ src/classes/apis/UserApi.ts
@@ -5,6 +5,8 @@ import type {
   TLoginZodSchema,
   TOtpZodSchema,
   TPasswordsZodSchema,
+  TRegisterZodSchema,
+  TSetPasswordZodSchema,
 } from '@beautinique/frontend-types';
 
 import { API_METHODS_AND_URLS } from '@/constants/api.constants';
@@ -14,6 +16,35 @@ import { ApiRequest } from '../ApiRequest';
 
 export class AuthApi extends ApiRequest {
   private routes = API_METHODS_AND_URLS.user_service.auth;
+
+  /* ===================== REGISTER API ===================== */
+
+  public registerSendOtp = (data: TEmailZodSchema) => {
+    return this.request({ ...this.routes.register.sendOtp, data });
+  };
+
+  public registerResendOtp = (token: string) => {
+    return this.request({
+      ...this.routes.register.resendOtp,
+      headers: { Authorization: `Bearer ${token}` },
+    });
+  };
+
+  public registerVerifyOtp = ({ token, ...data }: TOtpZodSchema & { token: string }) => {
+    return this.request({
+      ...this.routes.register.verifyOtp,
+      data,
+      headers: { Authorization: `Bearer ${token}` },
+    });
+  };
+
+  public registerSaveUser = ({ token, ...data }: TRegisterZodSchema & { token: string }) => {
+    return this.request({
+      ...this.routes.register.saveUser,
+      data,
+      headers: { Authorization: `Bearer ${token}` },
+    });
+  };
 
   /* ===================== LOGIN API ===================== */
 
@@ -56,6 +87,10 @@ export class AuthApi extends ApiRequest {
 
   public changePassword = (data: TChangePasswordZodSchema) => {
     return this.request<IUser>({ ...this.routes.password.change, data });
+  };
+
+  public setPassword = (data: TSetPasswordZodSchema) => {
+    return this.request({ ...this.routes.password.set, data });
   };
 
   /* ===================== LOGOUT API ===================== */
