68732f6c6e977927c9a3dad5d5dd9928a2c89cfb
Nageshwar <nageshpawarpatil@gmail.com>
2026-07-27 15:15:43 +0530

    fix: adjust z-index and height for sidebar component

--- src/components/layout/sidebar/index.tsx
+++ src/components/layout/sidebar/index.tsx
@@ -55,7 +55,7 @@ const Sidebar = () => {
   };
 
   return (
-    <aside className="border-t-silver/30 md:border-r-silver/30 bg-primary-invert sticky bottom-0 left-0 z-50 flex w-dvw items-center justify-center gap-4 border-t px-2 py-4 md:top-0 md:bottom-auto md:h-dvh md:w-fit md:flex-col md:border-r md:border-t-transparent">
+    <aside className="border-t-silver/30 md:border-r-silver/30 bg-primary-invert sticky bottom-0 left-0 z-40 flex w-dvw items-center justify-center gap-4 border-t px-2 py-4 md:top-16 md:bottom-auto md:h-[calc(100dvh-4rem)] md:w-fit md:flex-col md:border-r md:border-t-transparent">
       <ScrollableGradientContainer
         direction={isMobile ? 'horizontal' : 'vertical'}
         className="[&>div]:items-center [&>div]:justify-start"
